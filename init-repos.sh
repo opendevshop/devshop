98249ee7e3b73a24cf47f6b34d685bdf0b792110
Jon Pugh <jon@thinkdrop.net>
2014-02-02 18:59:30 -0500

    making repos folder and only running if the file doesnt exist

--- init-repos.sh
+++ init-repos.sh
@@ -1,6 +1,13 @@
 
 # DevShop Repos
-git clone http://git.drupal.org/project/devshop.git
-git clone http://git.drupal.org/project/devshop_provision.git
-git clone http://git.drupal.org/project/devshop_hosting.git
-git clone http://git.drupal.org/project/provision_git.git
+if [ ! -d 'repos' ]
+  then
+
+    mkdir repos
+    cd repos
+
+    git clone http://git.drupal.org/project/devshop.git
+    git clone http://git.drupal.org/project/devshop_provision.git
+    git clone http://git.drupal.org/project/devshop_hosting.git
+    git clone http://git.drupal.org/project/provision_git.git
+fi
