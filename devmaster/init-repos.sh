1ac1568b0d9e1ed2af9e4446b29dc44d8420f786
Jon Pugh <jon@thinkdrop.net>
2014-02-02 18:57:04 -0500

    adding repo init script

---
+++ devmaster/init-repos.sh
@@ -0,0 +1,6 @@
+
+# DevShop Repos
+git clone http://git.drupal.org/project/devshop.git
+git clone http://git.drupal.org/project/devshop_provision.git
+git clone http://git.drupal.org/project/devshop_hosting.git
+git clone http://git.drupal.org/project/provision_git.git
