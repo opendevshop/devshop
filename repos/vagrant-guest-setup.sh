884b4b4dc1f7950f02077e2a5a6471fa120cea91
Jon Pugh <jon@thinkdrop.net>
2014-03-31 00:34:54 -0400

    Create symlinks as aegir user.

--- repos/vagrant-guest-setup.sh
+++ repos/vagrant-guest-setup.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
-
+sudo su - aegir -c "
 rm -rf /var/aegir/.drush/devshop_provision
 ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
 
 rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/contrib/devshop_hosting
-ln -s /repos/devshop_provision /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/contrib/devshop_hosting
+ln -s /repos/devshop_provision /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop_hosting
 
 rm -rf /var/aegir/.drush/provision_git
 ln -s /repos/provision_git /var/aegir/.drush/provision_git
+"
