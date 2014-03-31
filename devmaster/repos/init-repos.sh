feed2ddeee5b056ecbfbcfb5919b3f06be63bfff
Jon Pugh <jon@thinkdrop.net>
2014-03-31 00:15:09 -0400

    Fixing git clone call.

--- devmaster/repos/init-repos.sh
+++ devmaster/repos/init-repos.sh
@@ -8,10 +8,10 @@ if [ -z "$1" ]
 fi
 
 # DevShop Repos
-`git clone $1@git.drupal.org:project/devshop.git`
-`git clone $1@git.drupal.org:project/devshop_provision.git`
-`git clone $1@git.drupal.org:project/devshop_hosting.git`
-`git clone $1@git.drupal.org:project/provision_git.git`
+git clone $1@git.drupal.org:project/devshop.git
+git clone $1@git.drupal.org:project/devshop_provision.git
+git clone $1@git.drupal.org:project/devshop_hosting.git
+git clone $1@git.drupal.org:project/provision_git.git
 
 # Run vagrant-guest-setup.sh on the guest machine
 vagrant ssh -c 'sudo sh /vagrant/repos/vagrant-guest-setup.sh'
