29ff55bf1028df1bb22ca29cb291c6cb1275bfbd
Jon Pugh <jon@thinkdrop.net>
2014-04-08 23:11:03 -0400

    Fixing location of devshop folder in vagrant guest setup

--- repos/vagrant-guest-setup.sh
+++ repos/vagrant-guest-setup.sh
@@ -3,7 +3,7 @@ sudo su - aegir -c "
 rm -rf /var/aegir/.drush/devshop_provision
 ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
 
-rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/contrib/devshop_hosting
+rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/contrib/devshop/devshop_hosting
 ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop_hosting
 
 rm -rf /var/aegir/.drush/provision_git
