51e70c9204c65e51582f1e1cc0caa18b0185ada4
Jon Pugh <jon@thinkdrop.net>
2014-04-11 11:36:09 -0400

    fixing folder replacement.

--- devmaster/repos/vagrant-guest-setup.sh
+++ devmaster/repos/vagrant-guest-setup.sh
@@ -3,8 +3,8 @@ sudo su - aegir -c "
 rm -rf /var/aegir/.drush/devshop_provision
 ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
 
-rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/contrib/devshop/devshop_hosting
-ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop_hosting
+rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
+ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
 
 rm -rf /var/aegir/.drush/provision_git
 ln -s /repos/provision_git /var/aegir/.drush/provision_git
