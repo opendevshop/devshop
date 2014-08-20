1437ab0e1a5cedf8750956fb77418579197d0b42
Jon Pugh <jon@thinkdrop.net>
2014-08-20 11:46:17 -0400

    Removing old teaser code.

--- devmaster/themes/boots/node-project.tpl.php
+++ devmaster/themes/boots/node-project.tpl.php
@@ -200,30 +200,3 @@
     <a href="/node/<?php print $node->nid; ?>/project_devshop-create" class="btn btn-lg btn-success"><i class="glyphicon glyphicon-plus"></i> <?php print t('Create New Environment'); ?></a>
   </div>
 </div>
-
-<div class="well">
-  <div id="node-<?php print $node->nid; ?>" class="node<?php if ($sticky) { print ' sticky'; } ?><?php if (!$status) { print ' node-unpublished'; } ?> clear-block">
-
-    <?php print $picture ?>
-
-    <?php if (!$page): ?>
-      <h2><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
-    <?php endif; ?>
-
-    <div class="meta">
-      <?php if ($submitted): ?>
-        <span class="submitted"><?php print $submitted ?></span>
-      <?php endif; ?>
-
-      <?php if ($terms): ?>
-        <div class="terms terms-inline"><?php print $terms ?></div>
-      <?php endif;?>
-    </div>
-
-    <div class="content">
-      <?php print $content ?>
-    </div>
-
-    <?php print $links; ?>
-  </div>
-</div>
