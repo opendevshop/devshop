cff59194932567232a74c4e5a03b123e4bef2c56
Jon Pugh <jon@thinkdrop.net>
2014-11-23 13:42:38 -0500

    Renaming preparation scripts and adding more cmments

--- vagrant-prepare-host.sh
+++ vagrant-prepare-host.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
+#
+# vagrant-prepare-host.sh
+#
+# Runs on your local machine (the vagrant host) to prepare source code for editing.
+# This script is run automatically on `vagrant up`.  You do not need to this manually.
+#
+
+# Passed argument
 cd $1
+
+# Build a full devshop frontend on the host with drush make, with working-copy option.
 if [ ! -d devshop-6.x-1.x ]; then
    drush make build-devshop.make devshop-6.x-1.x --working-copy --no-gitinfofile
    cp devshop-6.x-1.x/sites/default/default.settings.php devshop-6.x-1.x/sites/default/settings.php
@@ -7,6 +17,7 @@ if [ ! -d devshop-6.x-1.x ]; then
    chmod 777 devshop-6.x-1.x/sites/devshop.local
 fi
 
+# Clone drush packages.
 if [ ! -d drush ]; then
     mkdir drush
     git clone git@git.drupal.org:project/provision.git drush/provision --branch 6.x-2.x
