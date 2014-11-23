e0237b7a5127005ba221628070abffa29c47623a
Jon Pugh <jon@thinkdrop.net>
2014-11-23 12:02:01 -0500

    After ansible, prepare the dev VM by adding www-data to the aegir group.

--- devmaster/prepare-development-vagrant.sh
+++ devmaster/prepare-development-vagrant.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-sudo su - aegir -c "
-  rm -rf /var/aegir/.drush/devshop_provision
-  ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
+sudo adduser www-data aegir
+sudo service apache2 restart
 
-  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
-  ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
-
-  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
-  ln -s /repos/devshop/themes/boots /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
-
-  rm -rf /var/aegir/.drush/provision_git
-  ln -s /repos/provision_git /var/aegir/.drush/provision_git
-  "
+#sudo su - aegir -c "
+#  rm -rf /var/aegir/.drush/devshop_provision
+#  ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
+#
+#  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
+#  ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
+#
+#  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
+#  ln -s /repos/devshop/themes/boots /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
+#
+#  rm -rf /var/aegir/.drush/provision_git
+#  ln -s /repos/provision_git /var/aegir/.drush/provision_git
+#  "
