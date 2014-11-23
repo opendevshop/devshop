6fe1b9c3bd0fce95fcafc43fe3409d1757b0df39
Jon Pugh <jon@thinkdrop.net>
2014-11-23 13:31:21 -0500

    Adding comment about the vagrant environment.

--- devmaster/prepare-development-vagrant.sh
+++ devmaster/prepare-development-vagrant.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
+
+# We must add www-data to the aegir group in vagrant because the synced folder
+# /var/aegir/devshop-6.x-1.x (devshop front-end) is owned by aegir.
 sudo adduser www-data aegir
 sudo service apache2 restart
-
-#sudo su - aegir -c "
-#  rm -rf /var/aegir/.drush/devshop_provision
-#  ln -s /repos/devshop_provision /var/aegir/.drush/devshop_provision
-#
-#  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
-#  ln -s /repos/devshop_hosting /var/aegir/devshop-6.x-1.x/profiles/devshop/modules/devshop/devshop_hosting
-#
-#  rm -rf /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
-#  ln -s /repos/devshop/themes/boots /var/aegir/devshop-6.x-1.x/profiles/devshop/themes/boots
-#
-#  rm -rf /var/aegir/.drush/provision_git
-#  ln -s /repos/provision_git /var/aegir/.drush/provision_git
-#  "
