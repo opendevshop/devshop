91ab9d2f3409882bf616ecc43c4cf9dddafebd4a
Jon Pugh <jon@thinkdrop.net>
2014-11-23 14:40:22 -0500

    Removing provision_solr until upgraded to 2.x, it breaks any drush command.

--- vagrant-prepare-host.sh
+++ vagrant-prepare-host.sh
@@ -31,6 +31,5 @@ if [ ! -d drush ]; then
     git clone git@git.drupal.org:project/provision_git.git --branch 6.x-1.x
     git clone git@git.drupal.org:project/devshop_provision.git --branch 6.x-1.x
     git clone git@git.drupal.org:project/provision_logs.git --branch 6.x-1.x
-    git clone git@git.drupal.org:project/provision_solr.git --branch 6.x-1.x
     git clone git@git.drupal.org:project/provision_tasks_extra.git --branch 6.x-1.x
 fi
