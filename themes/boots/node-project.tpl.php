a3df111224411577c527836494b6e0c22a2f37a8
Jon Pugh <jon@thinkdrop.net>
2014-12-17 06:36:12 -0500

    fixing progress bar.

--- themes/boots/node-project.tpl.php
+++ themes/boots/node-project.tpl.php
@@ -223,7 +223,7 @@
 
 
         <div class="progress">
-          <div class="progress-bar <?php print $environment->progress_classes ?>"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style="width: 100%">
+          <div class="progress-bar progress-bar-striped progress-bar-warning active"  role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
             <span class="sr-only"><?php print $environment->progress_output ?></span>
           </div>
         </div>
