cbb8178a3d171dea6ff57d504ca75bdd19d4e548
Jon Pugh <jon@thinkdrop.net>
2015-01-21 09:37:52 -0500

    Adding test results to a modal window.

--- devmaster/themes/boots/node-project.tpl.php
+++ devmaster/themes/boots/node-project.tpl.php
@@ -372,16 +372,29 @@
         <div class="environment-tests list-group-item list-group-item-<?php print $environment->test->status_class ?>">
           <label><?php print t('Tests'); ?></label>
           <div class="btn-group btn-toolbar" role="toolbar">
-            <a href="<?php print $environment->test->status_url; ?>" type="button" class="btn">
+            <button type="button" class="btn" data-toggle="modal" data-target="#test-results-modal-<?php print $environment->name; ?>" title="<?php print t('View Results'); ?>">
               <?php print $environment->test->status_message ?>
 
               <small>
                 <?php print $environment->test->duration ?>
               </small>
-            </a>
+            </button>
 
 
-
+            <!--- TEST RESULTS MODAL -->
+            <div class="modal fade" id="test-results-modal" tabindex="-1" role="dialog" aria-labelledby="test-results-modal-<?php print $environment->name; ?>" aria-hidden="true">
+              <div class="modal-dialog modal-lg">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                    <h4 class="modal-title" id="drush-alias-modal"><?php print t('Test Results'); ?></h4>
+                  </div>
+                  <div class="modal-body">
+                    <?php print $environment->test->results; ?>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           <div class="btn-group" role="group">
 
