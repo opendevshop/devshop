adbc6165da1065142fcda15932374191a41ffcd8
Jon Pugh <jon@thinkdrop.net>
2015-02-13 13:18:52 -0500

    Fixing comments, and methods.

--- devmaster/Provision/Service/provider.php
+++ devmaster/Provision/Service/provider.php
@@ -1,27 +1,27 @@
 <?php
 
 /**
- * The site_project service class.
+ * The Provision_Service_provider service class.
  */
 class Provision_Service_provider extends Provision_Service {
   public $service = 'provider';
 
   /**
-   * Add environment, project, and git ref to site aliases.
+   * Add provider and provider options to the server drush alias.
    */
   static function subscribe_server($context) {
     $context->setProperty('provider');
     $context->setProperty('provider_options');
   }
 
-  function verify() {
-    $this->type_invoke('verify');
-    drush_log('[DEVSHOP] verify()', 'ok');
+  function verify_server_cmd() {
+    drush_log('[DEVSHOP] Verifying Server...' . d()->remote_host, 'ok');
   }
 
-  function verify_server_cmd() {
-    $this->create_config(d()->type);
-
-    $this->parse_configs();
+  static function option_documentation() {
+    return array(
+      '--provider' => 'The provider of this server. Must match an available Provision_Service_provider',
+      '--provider_options' => 'An array of options to send to the provider.',
+    );
   }
 }
