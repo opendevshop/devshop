8384f9729beb21cdca786bbfe5b206f3ffe068d6
Jon Pugh <jon@thinkdrop.net>
2015-02-13 16:09:05 -0500

    Adding verify server cmd for softlayer.

--- devmaster/Provision/Service/provider/softlayer.php
+++ devmaster/Provision/Service/provider/softlayer.php
@@ -17,6 +17,36 @@ class Provision_Service_provider_softlayer extends Provision_Service_provider {
 
     // Call Provision_Service_provider::save_server() to do things like save the IP address.
     parent::save_server();
+  }
 
+  /**
+   * Implements verify_server_cmd()
+   */
+  function verify_server_cmd(){
+
+    $server_fqdn = d()->remote_host;
+    $provider_options = (object) drush_get_option('provider_options', '');
+
+    drush_log('[DEVSHOP|softlayer] Verifying Server ' . $server_fqdn . '...', 'notice');
+//    drush_log('[DEVSHOP|softlayer] ' .print_r($provider_options, 1), 'notice');
+
+    require_once dirname(__FILE__) . '/SoftLayer/softlayer-api-php-client/SoftLayer/SoapClient.class.php';
+
+    $apiUsername = drush_get_option('softlayer-api-username', '');
+    $apiKey = drush_get_option('softlayer-api-key', '');
+
+
+    // Initialize an API client for the SoftLayer_Account service.
+    $hardware = SoftLayer_SoapClient::getClient('SoftLayer_Hardware', null, $apiUsername, $apiKey);
+
+    // Retrieve our account record
+    try {
+      $server = $hardware->createObject($provider_options);
+      print_r($server);
+
+    } catch (Exception $e) {
+
+      return drush_set_error('DEVSHOP_CLOUD_API_ACCESS_DENIED', $e->getMessage());
+    }
   }
 }
