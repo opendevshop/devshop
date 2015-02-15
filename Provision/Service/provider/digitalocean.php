d157959afc56a0c96584b13222ffa7f6fed3f54b
Jon Pugh <jon@thinkdrop.net>
2015-02-15 12:05:23 -0500

    Getting save_server() for digital ocean to work with fake data.

--- Provision/Service/provider/digitalocean.php
+++ Provision/Service/provider/digitalocean.php
@@ -5,18 +5,35 @@ class Provision_Service_provider_digitalocean extends Provision_Service_provider
   protected $provider = 'digitalocean';
 
   /**
-   * This method is called once per server verify.
+   * This method is called once per server verify, triggered from hosting task.
+   *
+   * $this->server: Provision_Context_server
    */
   function save_server() {
-    drush_log('[DEVSHOP|digitalocean] Provision_Service_provider_digitalocean->save_server()', 'ok');
 
-    // Save digitalocean specific info to the drush alias.
-    $this->server->setProperty('drop_id', 'FAKEDROPID');
+    // Look for provider_server_identifier
+    $server_identifier = $this->server->provider_server_identifier;
 
-    // $this->server->shell_exec($path . ' -V');
+    // If server ID is already found, move on.
+    if ($server_identifier) {
+      drush_log('[DEVSHOP] Server Identifier Found.  Not creating new server.', 'ok');
+    }
+    // If there is no sever ID, create the server.
+    else {
+
+      drush_log('[DEVSHOP] Server Identifier not found.  Creating new server!', 'ok');
+
+      // Faking our provider_data response.
+      $this->server->setProperty('provider_data', array(
+        'hello' => 'do',
+        'fake data' => 'from digitalocean',
+      ));
+
+      // Faking our provider server identifier.
+      $this->server->setProperty('provider_server_identifier', '123456789');
+    }
 
     // Call Provision_Service_provider::save_server() to do things like save the IP address.
     parent::save_server();
-
   }
 }
