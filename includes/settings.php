d12b5eb8055c3474c3ca6f1daba9ef3baa2518b7
Jon Pugh <jon@thinkdrop.net>
2015-02-15 20:26:55 -0500

    divs and typos.

--- includes/settings.php
+++ includes/settings.php
@@ -3,7 +3,9 @@
 function devshop_cloud_settings_form() {
   $form = array();
   $form['note'] = array(
-    '#value' => 'You must enable at least one cloud provider module.',
+    '#value' => t('You must enable at least one cloud provider module.'),
+    '#prefix' => '<div>',
+    '#suffix' => '</div>',
   );
   return system_settings_form($form);
 }
