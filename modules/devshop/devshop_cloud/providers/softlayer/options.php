8b0ea549057d60a326d742c036edfdb75b33cf2e
Jon Pugh <jon@thinkdrop.net>
2015-02-16 09:42:46 -0500

    Fixing up softlayer form to check for API key.

--- modules/devshop/devshop_cloud/providers/softlayer/options.php
+++ modules/devshop/devshop_cloud/providers/softlayer/options.php
@@ -7,24 +7,38 @@
 function devshop_softlayer_options_form() {
   $form = array();
 
-  $options = variable_get('devshop_cloud_softlayer_options', array());
+  $username = variable_get('devshop_cloud_softlayer_api_username', array());
+  $key = variable_get('devshop_cloud_softlayer_api_key', array());
 
-  $form['info'] = array(
-    '#type' => 'item',
-    '#title' => t('SoftLayer Options'),
-    '#value' => empty($options)? t('No options available. Click "Refresh SoftLayer Options".'): print_r($options, 1),
-  );
+  if (empty($username) || empty($key)) {
+    $form['warning'] = array(
+      '#prefix' => '<div class="alert alert-danger">',
+      '#suffix' => '</div>',
+      '#value' => t('You must enter your softlayer username and API key before you can use this form.  See !link', array('!link' => l(t('Cloud Settings'), 'admin/hosting/devshop/cloud'))),
+      '#weight' => 10,
+    );
+  }
+  else {
+    $options = variable_get('devshop_cloud_softlayer_options', array());
 
-  $form['note'] = array(
-    '#prefix' => '<div>',
-    '#suffix' => '</div>',
-    '#value' => t('Use the button below to retrieve available options from SoftLayer.'),
-  );
+    $form['info'] = array(
+      '#type' => 'item',
+      '#title' => t('SoftLayer Options'),
+      '#value' => empty($options)? t('No options available. Click "Refresh SoftLayer Options".'): print_r($options, 1),
+    );
 
-  $form['submit'] = array(
-    '#type' => 'submit',
-    '#value' => t('Refresh SoftLayer Options'),
-  );
+    $form['note'] = array(
+      '#prefix' => '<div>',
+      '#suffix' => '</div>',
+      '#value' => t('Use the button below to retrieve available options from SoftLayer.'),
+    );
+
+    $form['submit'] = array(
+      '#type' => 'submit',
+      '#value' => t('Refresh SoftLayer Options'),
+    );
+  }
+
   return $form;
 }
 
