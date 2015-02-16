8b026692c78f38bd9401cc0b55c2a608b512a4fb
Jon Pugh <jon@thinkdrop.net>
2015-02-15 21:23:19 -0500

    Moving verify_server_cmd to provider.php.

--- devmaster/Provision/Service/provider/digitalocean.php
+++ devmaster/Provision/Service/provider/digitalocean.php
@@ -18,7 +18,7 @@ class Provision_Service_provider_digitalocean extends Provision_Service_provider
     if ($server_identifier) {
       drush_log('[DEVSHOP] Server Identifier Found.  Not creating new server.', 'ok');
     }
-    // If there is no sever ID, create the server.
+    // If there is no server ID, create the server.
     else {
 
       drush_log('[DEVSHOP] Server Identifier not found.  Creating new server!', 'ok');
@@ -35,19 +35,6 @@ class Provision_Service_provider_digitalocean extends Provision_Service_provider
       $this->server->setProperty('ip_addresses', array(
         '1.2.3.4'
       ));
-    }
-  }
-
-  /**
-   * Saves server options to drush options so they will be picked up by
-   * devshop_cloud_post_hosting_verify_task()
-   */
-  function verify_server_cmd() {
-    drush_set_option('provider_data', $this->server->provider_data);
-    drush_set_option('provider_server_identifier', $this->server->provider_server_identifier);
-
-    if (!empty($this->server->ip_addresses)) {
-      drush_set_option('ip_addresses', $this->server->ip_addresses);
     }
   }
 }
