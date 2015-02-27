db0bb0083a1d2a28612eeb0045f6bc947afcf349
Jon Pugh <jon@thinkdrop.net>
2015-02-27 13:21:14 -0500

    Improving Tests UI

--- devmaster/themes/boots/node-project.tpl.php
+++ devmaster/themes/boots/node-project.tpl.php
@@ -385,10 +385,9 @@
               <?php print $environment->test->status_message ?>
 
               <small>
-                <?php print $environment->test->duration ?>
-                <br />
+                <?php print $environment->test->ago ?>
                 <em>
-                  <?php print $environment->test->ago ?>
+                  <?php print $environment->test->duration ?>
                 </em>
               </small>
             </button>
@@ -408,10 +407,10 @@
               </div>
             </div>
           </div>
-          <div class="btn-group" role="group">
-
-            <a href="<?php print $environment->test->run_tests_url; ?>" type="button" class="btn">
-              <?php print t('Run Tests'); ?>
+          <!-- Run Tests Button -->
+          <div class="btn-group pull-right" role="group">
+            <a href="<?php print $environment->test->run_tests_url; ?>" type="button" class="btn" title="<?php print t('Run Tests'); ?>">
+              <i class="fa fa-list"></i>
               <i class="fa fa-caret-right"></i>
             </a>
           </div>
