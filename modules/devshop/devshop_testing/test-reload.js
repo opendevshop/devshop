f47ae2aa65475ccac8a92f4be341716e01611253
Jon Pugh <jon@thinkdrop.net>
2015-04-01 23:51:03 -0400

    Follow Logs ability!

--- modules/devshop/devshop_testing/test-reload.js
+++ modules/devshop/devshop_testing/test-reload.js
@@ -1,11 +1,29 @@
 
+
     setInterval(function(){
-        console.log('loading results...');
-        console.log(Drupal.settings.test_url);
-        if (Drupal.settings.test_url) {
-            $('.results-wrapper').load(Drupal.settings.test_url, function () {
-                window.scrollTo(0,document.body.scrollHeight);
-            });
+        if (Drupal.settings.test_running == 'FALSE') {
+            return;
         }
+
+        $.get(Drupal.settings.test_status_url, function( running ) {
+          if (running == 'TRUE') {
+
+              console.log('Still running...');
+              $('.results-wrapper').load(Drupal.settings.test_url, function () {
+
+                  if ($('#follow').prop('checked')) {
+                      console.log('checked!');
+                      window.scrollTo(0,document.body.scrollHeight);
+                  }
+                  console.log('not checked!');
+
+              });
+          }
+          else {
+              console.log('Test Ended!');
+              Drupal.settings.test_running = 'FALSE';
+              $('.follow-checkbox ').hide();
+          }
+        });
     }, 1000);
 
