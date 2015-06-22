4aa709b4c81a3483062b33a31cded53bd6d97b20
Jon Pugh <jon@thinkdrop.net>
2015-06-22 12:29:29 -0400

    Nice little status command.

--- src/DevShop/Command/Status.php
+++ src/DevShop/Command/Status.php
@@ -8,6 +8,8 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
+use Symfony\Component\Process\Process;
+
 class Status extends Command
 {
   protected function configure()
@@ -21,5 +23,46 @@ class Status extends Command
   protected function execute(InputInterface $input, OutputInterface $output)
   {
     $output->writeln('Hello, DevShop!');
+
+    // Check for Drush
+    $output->write("<comment>Checking for Drush...  </comment>");
+
+    $process = new Process('drush --version');
+    $process->run();
+    if (!$process->isSuccessful()) {
+      $output->writeln("<question>Drush not detected.</question>");
+      $output->writeln($process->getErrorOutput());
+    }
+    else {
+      $output->write("<info>Drush is installed.</info>");
+      $output->write($process->getOutput());
+    }
+
+    // Check for provision
+    $output->write("<comment>Checking for Provision...  </comment>");
+
+    $process = new Process('drush help provision-save');
+    $process->run();
+    if (!$process->isSuccessful()) {
+      $output->writeln("<error>Provision not detected.</error>");
+      $output->writeln($process->getErrorOutput());
+    }
+    else {
+      $output->writeln("<info>Provision is installed.</info>");
+    }
+
+    // Check for devmaster
+    $output->write("<comment>Checking for DevMaster...  </comment>");
+
+    $process = new Process('drush @hostmaster status');
+    $process->run();
+    if (!$process->isSuccessful()) {
+      $output->writeln("<error>Devmaster not detected.</error>");
+      $output->writeln($process->getErrorOutput());
+    }
+    else {
+      $output->writeln("<info>Devmaster is installed.</info>");
+    }
+
   }
 }
