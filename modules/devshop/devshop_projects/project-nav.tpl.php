d0de099d2fa614ee6479f018c43dcc2eaa4fa0cc
Jon Pugh <jon@thinkdrop.net>
2015-08-20 22:15:38 -0400

    Better github icon.

--- modules/devshop/devshop_projects/project-nav.tpl.php
+++ modules/devshop/devshop_projects/project-nav.tpl.php
@@ -58,9 +58,9 @@
 
         <!-- Link to github or an icon -->
         <?php if ($project->git_provider == 'github'): ?>
-          <a class="input-group-addon" href="<?php print $project->git_repo_url; ?>" title="<?php print t('View on GitHub'); ?>" target="_blank"><i class="fa fa-github-alt"></i></a>
+          <a class="input-group-addon github-button" href="<?php print $project->git_repo_url; ?>" title="<?php print t('View on GitHub'); ?>" target="_blank"><i class="fa fa-github"></i></a>
         <?php elseif ($project->git_repo_url): ?>
-          <a class="input-group-addon" href="<?php print $project->git_repo_url; ?>" title="<?php print t('View Git Repo'); ?>" target="_blank"><i class="fa fa-git"></i></a>
+          <a class="input-group-addon large" href="<?php print $project->git_repo_url; ?>" title="<?php print t('View Git Repo'); ?>" target="_blank"><i class="fa fa-git"></i></a>
         <?php else: ?>
           <div class="input-group-addon"><i class="fa fa-git"></i></div>
         <?php endif; ?>
