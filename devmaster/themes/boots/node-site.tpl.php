f9d84b7b773199a51371e95ae8cd765935f13fb7
Jon Pugh <jon@thinkdrop.net>
2015-09-12 21:56:16 -0400

    Putting the site task links in a nice place.

--- devmaster/themes/boots/node-site.tpl.php
+++ devmaster/themes/boots/node-site.tpl.php
@@ -51,9 +51,10 @@
 
 <div class="container">
 <div class="row">
-    <div class="environment-wrapper col-sm-1 col-md-3 col-lg-3">
+    <div class="environment-wrapper col-sm-4 col-md-3 col-lg-3">
         <?php print theme('environment', $node->environment, $node->project, TRUE); ?>
-    <div class="col-md-4">
+    </div>
+    <div class="col-sm-4 col-md-4">
         <?php print theme("item_list", $environment->task_links, '', 'ul', array('class' => 'nav nav-pills nav-stacked')); ?>
     </div>
 </div>
