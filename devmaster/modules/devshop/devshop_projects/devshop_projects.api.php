ede73baa4e84a394448315fd1f94654ea0efdeb2
Jon Pugh <jon@thinkdrop.net>
2015-11-24 13:02:56 -0500

    Adding hook_devshop_environment_alter() and documentation.

--- devmaster/modules/devshop/devshop_projects/devshop_projects.api.php
+++ devmaster/modules/devshop/devshop_projects/devshop_projects.api.php
@@ -5,3 +5,13 @@
  * Example functions for interacting with devshop.
  */
 
+/**
+ * Implements hook_devshop_environment_alter()
+ *
+ * Activated from devshop_projects_load(), this runs anytime an environment is
+ * loaded.
+ *
+ */
+function hook_devshop_environment_alter(&$environment) {
+  $environment->tagline = t('Hosted by DevShop');
+}
