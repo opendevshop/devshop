a9059affe1e63f0b00ca3376899b7185e2ee2ca8
Jon Pugh <jon@thinkdrop.net>
2015-12-21 19:40:41 -0500

    Adding DevShop\Console\Command::getAnswer

--- src/DevShop/Console/Command.php
+++ src/DevShop/Console/Command.php
@@ -20,6 +20,8 @@ namespace DevShop\Console;
 use DevShop\DevShop;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\Question;
+
 use Symfony\Component\Console\Command\Command as BaseCommand;
 
 /**
@@ -79,5 +81,45 @@ abstract class Command extends BaseCommand
     $output->writeln(
       "╚{$bars}╝"
     );
+  }
+
+  /**
+   * Helper to ask a question only if a default argument is not present.
+   *
+   * @param InputInterface  $input
+   * @param OutputInterface $output
+   * @param Question        $question
+   *                                  A Question object
+   * @param $argument_name
+   *   Name of the argument or option to default to.
+   * @param string $type
+   *                     Either "argument" (default) or "option"
+   *
+   * @return mixed
+   *               The value derived from either the argument/option or the value.
+   */
+  public function getAnswer(InputInterface $input, OutputInterface $output, Question $question, $argument_name, $type = 'argument', $required = FALSE)
+  {
+    $helper = $this->getHelper('question');
+
+    if ($type == 'argument') {
+      $value = $input->getArgument($argument_name);
+    } elseif ($type == 'option') {
+      $value = $input->getOption($argument_name);
+    }
+
+    if (empty($value)) {
+
+      if ($required) {
+        while (empty($value)) {
+          $value = $helper->ask($input, $output, $question);
+        }
+      }
+      else {
+        $value = $helper->ask($input, $output, $question);
+      }
+    }
+
+    return $value;
   }
 }
