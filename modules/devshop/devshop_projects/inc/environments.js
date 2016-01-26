8955a023d301f594cd5369e0b0d3294dc8a63d9d
Andrew Rosborough <arosborough@dropforgelabs.com>
2016-01-26 14:33:08 -0500

    devshop_projects: Upgrade behaviors for inc/create/create.js and inc/environments.js.

--- modules/devshop/devshop_projects/inc/environments.js
+++ modules/devshop/devshop_projects/inc/environments.js
@@ -1,13 +1,16 @@
 
-
-Drupal.behaviors.devshopPlatforms = function() {
-  $('.form-option .description').hide();
-  $(".form-option").hover(
-    function() {
-        $(this).find(".description").show();
-    },
-    function() {
-        $(this).find(".description").hide();
+(function ($) {
+  Drupal.behaviors.devshopPlatforms = {
+    attach: (context, settings) {
+      $('.form-option .description').hide();
+      $(".form-option").hover(
+        function() {
+            $(this).find(".description").show();
+        },
+        function() {
+            $(this).find(".description").hide();
+        }
+      );
     }
-  );
-}
+  };
+}(jQuery));
