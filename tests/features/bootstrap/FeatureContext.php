a142f08fab04f56ea846b9e10662040048ff606d
Jon Pugh <jon@thinkdrop.net>
2016-01-26 00:22:27 -0500

    Extend Drush context so we can call drush commands.

--- tests/features/bootstrap/FeatureContext.php
+++ tests/features/bootstrap/FeatureContext.php
@@ -1,14 +1,14 @@
 <?php
 
-use Drupal\DrupalExtension\Context\RawDrupalContext;
+use Drupal\DrupalExtension\Context\DrushContext;
 use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\Gherkin\Node\PyStringNode;
 use Behat\Gherkin\Node\TableNode;
-
+use Symfony\Component\Process\Process;
 /**
  * Defines application features from the specific context.
  */
-class FeatureContext extends RawDrupalContext implements SnippetAcceptingContext {
+class FeatureContext extends DrushContext implements SnippetAcceptingContext {
 
   /**
    * Initializes context.
