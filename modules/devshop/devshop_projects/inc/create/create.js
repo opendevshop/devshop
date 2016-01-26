a5b4a3a5434334df08768dccda3453d7b0a40cd1
Andrew Rosborough <arosborough@dropforgelabs.com>
2016-01-26 14:23:52 -0500

    devshop_projects: Upgrade behaviors for inc/create/create.js.

--- modules/devshop/devshop_projects/inc/create/create.js
+++ modules/devshop/devshop_projects/inc/create/create.js
@@ -3,39 +3,48 @@
  *
  * Step 1: Takes the entered name and creates a base url and code path from it.
  */
-Drupal.behaviors.createStep1 = function() {
-    $( "#edit-title" ).keyup(function(event) {
+(function ($) {
+  Drupal.behaviors.createStep1 = {
+    attach: function (context, settings) {
 
-      // Extract project name and base path and URL
-      var projectName = $(this).val();
-      var base_path = $('#edit-code-path').attr('data-base_path') + '/' + projectName;
-      var base_url = projectName + '.' + $('#edit-base-url').attr('data-base_url');
+      $( "#edit-title" ).keyup(function(event) {
 
-      $('#edit-code-path').val(base_path);
-      $('#edit-base-url').val(base_url);
+        // Extract project name and base path and URL
+        var projectName = $(this).val();
+        var base_path = $('#edit-code-path').attr('data-base_path') + '/' + projectName;
+        var base_url = projectName + '.' + $('#edit-base-url').attr('data-base_url');
 
-    });
-}
+        $('#edit-code-path').val(base_path);
+        $('#edit-base-url').val(base_url);
+
+      });
+
+    }
+  };
+}(jQuery))
 
 /**
  * Step 2: Settings
  */
-Drupal.behaviors.createStep2 = function() {
+(function ($) {
+  Drupal.behaviors.createStep2 = {
+    attach: function (context, settings) {
 
-  // Hide unless
-  $('#edit-project-settings-live-live-domain-www-wrapper').hide();
-  $('#edit-project-settings-live-environment-aliases-wrapper').hide();
-
-
-  $('#edit-project-settings-live-live-domain').keyup(function(){
-    if ($(this).val()){
-      $('#edit-project-settings-live-live-domain-www-wrapper').show();
-      $('#edit-project-settings-live-environment-aliases-wrapper').show();
-    }
-    else {
+      // Hide unless
       $('#edit-project-settings-live-live-domain-www-wrapper').hide();
       $('#edit-project-settings-live-environment-aliases-wrapper').hide();
-    }
-  });
 
-}
+      $('#edit-project-settings-live-live-domain').keyup(function(){
+        if ($(this).val()){
+          $('#edit-project-settings-live-live-domain-www-wrapper').show();
+          $('#edit-project-settings-live-environment-aliases-wrapper').show();
+        }
+        else {
+          $('#edit-project-settings-live-live-domain-www-wrapper').hide();
+          $('#edit-project-settings-live-environment-aliases-wrapper').hide();
+        }
+      });
+
+    }
+  };
+}(jQuery))
