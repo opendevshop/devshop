17e976f2509aee1f1fba60a0e24abc5e347b9957
Jon Pugh <jon@thinkdrop.net>
2016-01-30 16:11:34 -0500

    Removing 'env' from supervisor stop call.

--- tests/docker-launch-devshop.sh
+++ tests/docker-launch-devshop.sh
@@ -17,7 +17,7 @@ docker pull $DISTRIBUTION:$VERSION
 docker build --rm=true --file=Dockerfile.$DISTRIBUTION-$VERSION --tag=$DISTRIBUTION-$VERSION:devmaster .
 docker run --detach -p $HOST_PORT:80 $RUN_OPTS --volume=$PWD/..:/etc/ansible/devshop:rw -h $CONTAINER_HOSTNAME $DISTRIBUTION-$VERSION:devmaster $INIT
 docker exec --tty $CONTAINER_NAME env TERM=xterm sudo su -c "/etc/ansible/devshop/install.sh $SCRIPT_OPTS --hostname=$CONTAINER_HOSTNAME"
-docker exec $CONTAINER_NAME env $SUPERVISOR_STOP
+docker exec $CONTAINER_NAME $SUPERVISOR_STOP
 docker exec $CONTAINER_NAME env sudo su - aegir -c "drush @hostmaster dis hosting_queued -y -v"
 
 bash docker-test-devshop.sh
