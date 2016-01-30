619673c4308ae18462fc79e0665b3d88c7c6a3db
Andrew Rosborough <arosborough@dropforgelabs.com>
2016-01-30 12:07:01 -0500

    Playbook will run build-devmaster.make from /tmp with travis_branch and travis_commit

--- install.sh
+++ install.sh
@@ -239,8 +239,10 @@ echo $LINE
 
 ANSIBLE_EXTRA_VARS="server_hostname=$HOSTNAME_FQDN mysql_root_password=$MYSQL_ROOT_PASSWORD playbook_path=$PLAYBOOK_PATH server_webserver=$SERVER_WEBSERVER"
 
-if [ $HOSTNAME_FQDN == "devshop.travis" ]; then
+if [ "$TRAVIS" == "true" ]; then
   ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS travis=true travis_repo_slug=$TRAVIS_REPO_SLUG travis_branch=$TRAVIS_BRANCH travis_commit=$TRAVIS_COMMIT"
+  curl $MAKEFILE_PATH > /tmp/build-devmaster.make
+  MAKEFILE_PATH="/tmp/build-devmaster.make"
 else
   ANSIBLE_EXTRA_VARS="$ANSIBLE_EXTRA_VARS travis=false"
 fi
