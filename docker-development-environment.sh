79af848a881fa77ea9a248904f5375af59f5cedb
Jon Pugh <jon@thinkdrop.net>
2016-02-04 16:44:47 -0500

    Fixing distribution version.

--- docker-development-environment.sh
+++ docker-development-environment.sh
@@ -22,13 +22,13 @@ cd tests
 composer install
 
 # Pulled from our .travis.yml
-docker pull $DISTRIBUTION:$VERSION
-docker build --rm=true --file=Dockerfile.$DISTRIBUTION-$VERSION --tag=$DISTRIBUTION-$VERSION:devmaster .
+docker pull $DISTRIBUTION:$DISTRIBUTION_VERSION
+docker build --rm=true --file=Dockerfile.$DISTRIBUTION-$DISTRIBUTION_VERSION --tag=$DISTRIBUTION-$DISTRIBUTION_VERSION:devmaster .
 docker run --detach -p $HOST_PORT:80 $RUN_OPTS \
     --volume=$PWD/..:/usr/share/devshop:rw \
-    --volume=$PWD/../source/devmaster-$VERSION:/var/aegir/devmaster-$VERSION \
+    --volume=$PWD/../source/devmaster-$DISTRIBUTION_VERSION:/var/aegir/devmaster-$DISTRIBUTION_VERSION \
     --volume=$PWD/../source/drush/commands:/var/aegir/.drush/commands \
-    -h $CONTAINER_HOSTNAME $DISTRIBUTION-$VERSION:devmaster $INIT
+    -h $CONTAINER_HOSTNAME $DISTRIBUTION-$DISTRIBUTION_VERSION:devmaster $INIT
 docker exec --tty $CONTAINER_NAME env TERM=xterm sudo su -c "/usr/share/devshop/install.sh $SCRIPT_OPTS --hostname=$CONTAINER_HOSTNAME"
 docker exec $CONTAINER_NAME $SUPERVISOR_STOP
 docker exec $CONTAINER_NAME env sudo su - aegir -c "drush @hostmaster dis hosting_queued -y -v"
