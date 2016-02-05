e2a260d07e92d9e009477e24d5fa79bde6d16767
Jon Pugh <jon@thinkdrop.net>
2016-02-05 18:00:38 -0500

    forgot to remove the chown.

--- docker-development-environment.sh
+++ docker-development-environment.sh
@@ -16,7 +16,6 @@ bash vagrant-prepare-host.sh $PWD $DEVSHOP_VERSION
 
 # Changing UID:GID of source code to Aegir's UID so it can write to these folders.
 # We can change it back to the user later so they can edit the files.
-sudo chown 12345:12345 source -R
 cd tests
 
 composer install
