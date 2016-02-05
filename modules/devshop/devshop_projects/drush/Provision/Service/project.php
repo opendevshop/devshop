ee1beb6c9c62734fd68893cc62b96e3a67c062ae
Jon Pugh <jon@thinkdrop.net>
2016-02-05 13:10:09 -0500

    Removing debug logging.

---
+++ modules/devshop/devshop_projects/drush/Provision/Service/project.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * The project service class.
+ */
+class Provision_Service_project extends Provision_Service {
+  public $service = 'project';
+
+  /**
+   * Add environment, project, and git ref to site aliases.
+   */
+  static function subscribe_site($context) {
+    drush_log('Provision_Service_platform_project::subscribe_platform()', 'devshop_log');
+    $context->setProperty('environment');
+    $context->setProperty('project');
+    $context->setProperty('git_ref');
+  }
+
+  /**
+   * Add environment, project, and git ref to platform aliases.
+   */
+  static function subscribe_platform($context) {
+    drush_log('Provision_Service_platform_project::subscribe_platform()', 'devshop_log');
+    $context->setProperty('environment');
+    $context->setProperty('project');
+    $context->setProperty('git_ref');
+  }
+
+}
