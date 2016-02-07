5f4e38e45f1ec2d0b6e26400d44c533eff04b381
Andrew Rosborough <arosborough@dropforgelabs.com>
2016-02-07 16:26:14 -0500

    Fix warnings on project list page and refactor intercomSettings to a file

--- themes/boots/template.php
+++ themes/boots/template.php
@@ -446,16 +446,7 @@ function boots_preprocess_page(&$vars){
 
   }
   if (variable_get('devshop_support_widget_enable', TRUE)) {
-    $vars['page']['page_bottom']['intercomSettings'] = array(
-      '#attached' => array('js' => array(
-"<script>
-  window.intercomSettings = {
-    app_id: \"ufeta82d\"
-  };
-</script>
-<script>(function(){var w=window;var ic=w.Intercom;if(typeof ic===\"function\"){ic('reattach_activator');ic('update',intercomSettings);}else{var d=document;var i=function(){i.c(arguments)};i.q=[];i.c=function(args){i.q.push(args)};w.Intercom=i;function l(){var s=d.createElement('script');s.type='text/javascript';s.async=true;s.src='https://widget.intercom.io/widget/ufeta82d';var x=d.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);}if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})()</script>"
-     => array('type' => 'inline')
-    )));
+    drupal_add_js(drupal_get_path('theme', 'boots') . '/js/intercomSettings.js', array('type' => 'file'));
   }
 }
 
