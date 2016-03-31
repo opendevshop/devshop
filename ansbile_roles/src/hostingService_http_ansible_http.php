77bc4ad2ed4c06097be21e2fd8f15d2600dcfc72
Jon Pugh <jon@thinkdrop.net>
2016-03-31 18:10:33 -0400

    Adding our own form function to http.

--- ansbile_roles/src/hostingService_http_ansible_http.php
+++ ansbile_roles/src/hostingService_http_ansible_http.php
@@ -11,24 +11,12 @@ class hostingService_http_ansible_http extends hostingService_http_public {
 
     protected $has_restart_cmd = TRUE;
 
-    function default_restart_cmd() {
-        $command = '/usr/sbin/apache2ctl'; # a proper default for most of the world
-        foreach (explode(':', $_SERVER['PATH']) as $path) {
-            $options[] = "$path/apache2ctl";
-            $options[] = "$path/apachectl";
-        }
-        # try to detect the apache restart command
-        $options[] = '/usr/local/sbin/apachectl'; # freebsd
-        $options[] = '/usr/sbin/apache2ctl'; # debian + apache2
-        $options[] = $command;
-
-        foreach ($options as $test) {
-            if (is_executable($test)) {
-                $command = $test;
-                break;
-            }
-        }
-
-        return "sudo $command graceful";
+    function form(&$form) {
+        parent::form($form);
+        $form['note'] = array(
+            '#markup' => t('Your web server will be configured automatically.'),
+            '#prefix' => '<p>',
+            '#suffix' => '</p>',
+        );
     }
 }
