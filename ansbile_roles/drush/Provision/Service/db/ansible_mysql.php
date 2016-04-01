d3d766cb31bf8c13ff7ad3b1a98a3dc1b729af56
Jon Pugh <jon@thinkdrop.net>
2016-03-31 20:03:07 -0400

    Extend Provision_Service() so we don't have to worry about database services.

--- ansbile_roles/drush/Provision/Service/db/ansible_mysql.php
+++ ansbile_roles/drush/Provision/Service/db/ansible_mysql.php
@@ -4,6 +4,6 @@
  * Provides the MySQL service driver.
  */
 
-class Provision_Service_db_ansible_mysql extends Provision_Service_db {
+class Provision_Service_db_ansible_mysql extends Provision_Service {
 
 }
