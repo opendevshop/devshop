6792f39d0119c945a7503e4da8e012295877f547
Jon Pugh <jon@thinkdrop.net>
2016-03-31 20:08:14 -0400

    Overriding sync() method and adding verify_%_cmd() methods.

--- ansbile_roles/drush/Provision/Service/http/ansible_apache.php
+++ ansbile_roles/drush/Provision/Service/http/ansible_apache.php
@@ -7,8 +7,21 @@
 class Provision_Service_http_ansible_apache extends Provision_Service_http {
 
     /**
-     * Override sync() method since we don't need to sync.
+     * Override sync() method because we don't need to sync.
      */
-    public function sync() {
+    function sync($path = NULL, $additional_options = array()) {
+//        return $this->server->sync($path, $additional_options);
+    }
+
+    function verify_server_cmd() {
+        drush_log('Server Verified', 'devshop_log');
+    }
+
+    function verify_platform_cmd() {
+        drush_log('Platform Verified', 'devshop_log');
+    }
+
+    function verify_site_cmd() {
+        drush_log('Site Verified', 'devshop_log');
     }
 }
