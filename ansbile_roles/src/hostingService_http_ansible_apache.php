57ef297b8d86b856d9a12e490691c79c270c8c3c
Jon Pugh <jon@thinkdrop.net>
2016-04-01 11:06:21 -0400

    Move ansible variable loading to ansible_apache service.

--- ansbile_roles/src/hostingService_http_ansible_apache.php
+++ ansbile_roles/src/hostingService_http_ansible_apache.php
@@ -6,17 +6,29 @@
 
 
 
-class hostingService_http_ansible_apache extends hostingService_http {
+class hostingService_http_ansible_apache extends hostingService_http
+{
     public $type = 'ansible_apache';
 
     protected $has_restart_cmd = FALSE;
 
-    function form(&$form) {
+    function form(&$form)
+    {
         parent::form($form);
         $form['note'] = array(
             '#markup' => t('Your web server will be configured automatically.'),
             '#prefix' => '<p>',
             '#suffix' => '</p>',
         );
+    }
+
+
+    /**
+     * Load Apache ansible variables.
+     */
+    function load()
+    {
+        parent::load();
+        $this->ansible_vars['aegir_user_authorized_keys'] = variable_get('devshop_public_key', '');
     }
 }
