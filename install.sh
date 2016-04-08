0f4066e22c32ff7be8e2da5084a88983b82cdf5e
Jon Pugh <jon@thinkdrop.net>
2016-04-07 21:09:04 -0400

    Don't mess with git outpu.

--- install.sh
+++ install.sh
@@ -177,7 +177,7 @@ if [ ! `which ansible > /dev/null 2>&1` ]; then
         yum install -y epel-release > /dev/null 1>&1
         yum install -y git rpm-build make asciidoc python-setuptools python2-devel PyYAML python-httplib2 python-jinja2 python-keyczar python-paramiko python-six sshpass > /dev/null 1>&1
 
-        git clone git://github.com/ansible/ansible.git --branch stable-2.0.0.1 --recursive  > /dev/null 1>&1
+        git clone git://github.com/ansible/ansible.git --branch stable-2.0.0.1 --recursive
         cd ./ansible
         make rpm  > /dev/null 1>&1
         rpm -Uvh ./rpm-build/ansible-*.noarch.rpm  > /dev/null 2>&1
