7bafd5ee39384efde1392b701844f6246a9e6641
Jon Pugh <jon@thinkdrop.net>
2016-04-11 21:14:41 -0400

    Fixing drush aliases

--- modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
+++ modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
@@ -3,19 +3,23 @@
  * @file
  * Template file for a project's alias file.
  */
-print "<?php // Automatically written by devshop. Do Not Edit. \n";
+print "<?php // Automatically written by devshop when a project is verified. Do Not Edit. \n";
 
 foreach ($environments as $name => $environment) {
   $alias = array(
     'root' => $environment['root'],
     'uri' => $environment['uri'],
-    'remote_host' => $environment['remote_host'],
+    'remote_host' => d('web_server')->remote_host,
     'remote_user' => d('web_server')->script_user,
+    'path-aliases' => array(
+      '%files' =>  "sites/{$environment['uri']}/files"
+    ),
   );
 
   $export = var_export($alias, TRUE);
   ?>
-  $aliases['<?php print $name; ?>'] = <?php print $export; ?>;
+
+$aliases['<?php print $name; ?>'] = <?php print $export; ?>;
 
   <?php
 }
