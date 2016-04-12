872a8647be46ce9f8d5a8d232fe7b03dda7ad658
Jon Pugh <jon@thinkdrop.net>
2016-04-11 20:56:36 -0400

    Save Drush aliases on Project Verify

---
+++ modules/devshop/devshop_projects/drush/Provision/Config/ProjectAliases.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * @file
+ * Provides the Provision_Config_Drushrc_Alias class.
+ */
+
+/**
+ * Class to write an alias records.
+ */
+class Provision_Config_ProjectAliases extends Provision_Config_Drushrc {
+  public $template = 'project_aliases.tpl.php';
+
+  /**
+   * @param $name
+   *   String '\@name' for named context.
+   * @param $options
+   *   Array of string option names to save.
+   */
+  function __construct($context, $environments = array()) {
+
+    parent::__construct($context, $environments);
+    $this->data = array(
+      'name' => ltrim($context, '@project_'),
+      'environments' => $environments,
+    );
+  }
+
+  function filename() {
+    return drush_server_home() . '/.drush/projects/' . $this->data['name'] . '.aliases.drushrc.php';
+  }
+}
