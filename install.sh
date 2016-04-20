a3d068dd33e326033b9b114caae31ac5532d210a
Jon Pugh <jon@thinkdrop.net>
2016-04-20 14:33:24 -0400

    Fixing install.sh version back to 1.x

--- install.sh
+++ install.sh
@@ -24,7 +24,7 @@
 
 # Version used for cloning devshop playbooks
 # Must be a branch or tag.
-DEVSHOP_VERSION=1.x-ansible
+DEVSHOP_VERSION=1.x
 SERVER_WEBSERVER=apache
 MAKEFILE_PATH=''
 AEGIR_USER_UID=12345
