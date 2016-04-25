abd95f0efecc7d2a82d98274b2273ffa70c44724
Jon Pugh <jon@thinkdrop.net>
2016-04-25 17:46:14 -0400

    Fixing the variable name to devshop_cloud_public_key.

--- includes/settings.php
+++ includes/settings.php
@@ -8,10 +8,10 @@ function devshop_cloud_settings_form() {
     '#suffix' => '</div>',
   );
   $devshop_key = variable_get('devshop_public_key');
-  $form['devshop_cloud_ssh_key'] = array(
+  $form['devshop_cloud_public_key'] = array(
     '#title' => t('DevShop Cloud Public SSH Key'),
     '#description' => t('Enter the public key of the user that will be connecting and provisioning your servers. This SSH key will be used to grant access to root on new servers created by DevShop Cloud.'),
-    '#default_value' => variable_get('devshop_cloud_ssh_key', $devshop_key),
+    '#default_value' => variable_get('devshop_cloud_public_key', $devshop_key),
     '#type' => 'textarea',
   );
   return system_settings_form($form);
