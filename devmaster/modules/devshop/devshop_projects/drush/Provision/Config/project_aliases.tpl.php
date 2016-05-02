49926a39b54e90da644d10dd8fd14aa47d3c6728
Jon Pugh <jon@thinkdrop.net>
2016-05-02 12:37:14 -0400

    Output "remote environments" into project drush aliases.

--- devmaster/modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
+++ devmaster/modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
@@ -5,7 +5,7 @@
  */
 print "<?php // Automatically written by devshop when a project is verified. Do Not Edit. \n";
 
-foreach ($environments as $name => $environment) {
+foreach ($project['environments'] as $name => $environment) {
 
   if ($environment['site_status'] != 1) {
     continue;
@@ -13,14 +13,22 @@ foreach ($environments as $name => $environment) {
   $alias = array(
     'root' => $environment['root'],
     'uri' => $environment['uri'],
-    'remote_host' => d('web_server')->remote_host,
-    'remote_user' => d('web_server')->script_user,
+    'remote-host' => d('web_server')->remote_host,
+    'remote-user' => d('web_server')->script_user,
     'path-aliases' => array(
       '%files' =>  "sites/{$environment['uri']}/files"
     ),
   );
 
   $export = var_export($alias, TRUE);
+  ?>
+
+$aliases['<?php print $name; ?>'] = <?php print $export; ?>;
+
+  <?php
+}
+foreach ($project['settings']['aliases'] as $name => $remote_alias) {
+  $export = var_export($remote_alias, TRUE);
   ?>
 
 $aliases['<?php print $name; ?>'] = <?php print $export; ?>;
