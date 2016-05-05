b7c192bf502e8984f704a1bf7f62641d7a6f1dd4
Jon Pugh <jon@thinkdrop.net>
2016-05-05 11:44:33 -0400

    Switching to command -v

--- install.sh
+++ install.sh
@@ -143,7 +143,8 @@ fi
 
 # If ansible command is not available, install it.
 # Decided on "hash" thanks to http://stackoverflow.com/questions/592620/check-if-a-program-exists-from-a-bash-script
-if [ ! hash ansible 2>/dev/null ]; then
+# After testing this thoroughly on centOS and ubuntu, I think we should use command -v
+if [ ! `command -v ansible` ]; then
     echo " Installing Ansible..."
 
     if [ $OS == 'ubuntu' ] || [ $OS == 'debian' ]; then
