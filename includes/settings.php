0ee182ee32401a3156125c3ffdc87c01c72122a2
Jon Pugh <jon@thinkdrop.net>
2016-05-17 01:15:07 -0400

    Fixing aegir cloud settings form.

--- includes/settings.php
+++ includes/settings.php
@@ -1,6 +1,6 @@
 <?php
 
-function devshop_cloud_settings_form() {
+function aegir_cloud_settings_form() {
   $form = array();
   $form['note'] = array(
     '#value' => t('You must enable at least one cloud provider module.'),
@@ -9,8 +9,8 @@ function devshop_cloud_settings_form() {
   );
   $devshop_key = variable_get('devshop_public_key');
   $form['devshop_cloud_public_key'] = array(
-    '#title' => t('DevShop Cloud Public SSH Key'),
-    '#description' => t('Enter the public key of the user that will be connecting and provisioning your servers. This SSH key will be used to grant access to root on new servers created by DevShop Cloud.'),
+    '#title' => t('Hostmaster Public SSH Key'),
+    '#description' => t('Enter the public key of the user that will be connecting and provisioning your servers (typically "aegir". This SSH key will be used to grant access to root on new servers created by Aegir Cloud.'),
     '#default_value' => variable_get('devshop_cloud_public_key', $devshop_key),
     '#type' => 'textarea',
   );
