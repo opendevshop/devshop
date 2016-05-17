149fae67d25c6e216be383b554d379c55f8df39a
Jon Pugh <jon@thinkdrop.net>
2016-05-17 17:12:38 -0400

    Fixing SSH public key variable names.

--- includes/settings.php
+++ includes/settings.php
@@ -7,11 +7,11 @@ function aegir_cloud_settings_form() {
     '#prefix' => '<div>',
     '#suffix' => '</div>',
   );
-  $devshop_key = variable_get('devshop_public_key');
-  $form['devshop_cloud_public_key'] = array(
+  $devshop_key = variable_get('aegir_public_key', '');
+  $form['aegir_public_key'] = array(
     '#title' => t('Hostmaster Public SSH Key'),
     '#description' => t('Enter the public key of the user that will be connecting and provisioning your servers (typically "aegir". This SSH key will be used to grant access to root on new servers created by Aegir Cloud.'),
-    '#default_value' => variable_get('devshop_cloud_public_key', $devshop_key),
+    '#default_value' => variable_get('aegir_public_key', $devshop_key),
     '#type' => 'textarea',
   );
   return system_settings_form($form);
