17a3a92338e18f93e0a3e6c1a3b71f7e0942ea1d
Jon Pugh <jon@thinkdrop.net>
2016-05-27 17:15:18 -0400

    Adding NGINX services for ansible.

---
+++ ansible_services/drush/Provision/Service/http/ansible_nginx.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once(__DIR__ . '/../../../../../vendor/autoload.php');
+
+use Asm\Ansible\Ansible;
+
+
+/**
+ * @file
+ * Provides the Ansible Apache service driver.
+ */
+
+class Provision_Service_http_ansible_nginx extends Provision_Service_http_nginx_ssl {
+}
