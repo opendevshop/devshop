a803dfc8818bf93c575f79a31b52d3d0b55aeab4
Jon Pugh <jon@thinkdrop.net>
2016-06-11 15:44:53 -0400

    Fixing the projects dashboard with matchHeight()

--- themes/boots/js/boots.js
+++ themes/boots/js/boots.js
@@ -1,7 +1,9 @@
 (function ($) {
+    Drupal.behaviors.devshopTasks = {
+        attach: function (context, settings) {
 
-    // Match the height of environments so the grid doesn't break.
-    $('.environment-wrapper').matchHeight();
-
-
+            // Match the height of environments so the grid doesn't break.
+            $('.environment-wrapper').matchHeight();
+        },
+    }
 })(jQuery);
