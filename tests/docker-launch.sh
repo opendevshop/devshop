72e20f47a3fc6c2fb4026cd9080385e37015adcc
Jon Pugh <jon@thinkdrop.net>
2016-06-14 15:56:35 -0400

    Removing the stopping of supervisor from docker-launch.sh

--- tests/docker-launch.sh
+++ tests/docker-launch.sh
@@ -7,7 +7,9 @@ CONTAINER_NAME='devshop_server'
 RUN_OPTS="--name=$CONTAINER_NAME"
 SCRIPT_OPTS='--server-webserver=nginx'
 CONTAINER_HOSTNAME=devshop.docker
-SUPERVISOR_STOP='service supervisor stop'
+
+# Don't stop supervisor until we want to run tests.
+# SUPERVISOR_STOP='service supervisor stop'
 HOST_PORT=8000
 TRAVIS=true
 
@@ -18,7 +20,8 @@ docker pull $DISTRIBUTION:$VERSION
 docker build --rm=true --file=Dockerfile.$DISTRIBUTION-$VERSION --tag=$DISTRIBUTION-$VERSION:devmaster .
 docker run --detach -p $HOST_PORT:80 $RUN_OPTS --volume=$PWD/..:/usr/share/devshop:rw -h $CONTAINER_HOSTNAME $DISTRIBUTION-$VERSION:devmaster $INIT
 docker exec --tty $CONTAINER_NAME env TERM=xterm sudo su -c "/usr/share/devshop/install.sh $SCRIPT_OPTS --hostname=$CONTAINER_HOSTNAME"
-docker exec $CONTAINER_NAME $SUPERVISOR_STOP
-docker exec $CONTAINER_NAME env sudo su - aegir -c "drush @hostmaster dis hosting_queued -y -v"
 
-bash docker-test-devshop.sh
+# Don't stop queue until the user runs tests.
+# docker exec $CONTAINER_NAME $SUPERVISOR_STOP
+# docker exec $CONTAINER_NAME env sudo su - aegir -c "drush @hostmaster dis hosting_queued -y -v"
+# bash docker-test-devshop.sh
