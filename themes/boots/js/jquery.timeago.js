a0c58faebc23ea7ed7c41f7035130370690ed891
Jon Pugh <jon@thinkdrop.net>
2016-06-15 13:45:42 -0400

    Patching time ago to not cut to one minute at 45 seconds.

--- themes/boots/js/jquery.timeago.js
+++ themes/boots/js/jquery.timeago.js
@@ -99,14 +99,14 @@
         return string.replace(/%d/i, value);
       }
 
-      var words = seconds < 45 && substitute($l.seconds, Math.round(seconds)) ||
+      var words = seconds < 60 && substitute($l.seconds, Math.round(seconds)) ||
         seconds < 90 && substitute($l.minute, 1) ||
-        minutes < 45 && substitute($l.minutes, Math.round(minutes)) ||
+        minutes < 60 && substitute($l.minutes, Math.round(minutes)) ||
         minutes < 90 && substitute($l.hour, 1) ||
         hours < 24 && substitute($l.hours, Math.round(hours)) ||
         hours < 42 && substitute($l.day, 1) ||
         days < 30 && substitute($l.days, Math.round(days)) ||
-        days < 45 && substitute($l.month, 1) ||
+        days < 60 && substitute($l.month, 1) ||
         days < 365 && substitute($l.months, Math.round(days / 30)) ||
         years < 1.5 && substitute($l.year, 1) ||
         substitute($l.years, Math.round(years));
