c0d36f389bb64c8291b400edf90c6a99ac26d347
Jon Pugh <jon@thinkdrop.net>
2016-07-16 22:52:28 -0400

    Adding a prepare environment script, copied from vagrant-prepare-host.sh

---
+++ docker/development/prepare-environment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# This script prepares a development environment for DevShop,
+# cloning the source code you need.
+#
+
+DEVMASTER_VERSION=1.x
+
+# Build a full devshop frontend on the host with drush make, with working-copy option.
+if [ ! -d devmaster-$DEVMASTER_VERSION ]; then
+   drush make ../../build-devmaster.make devmaster-$DEVMASTER_VERSION --working-copy --no-gitinfofile
+   cp devmaster-$DEVMASTER_VERSION/sites/default/default.settings.php devmaster-$DEVMASTER_VERSION/sites/default/settings.php
+   mkdir devmaster-$DEVMASTER_VERSION/sites/devshop.local.computer
+   chmod 777 devmaster-$DEVMASTER_VERSION/sites -R
+fi
+
+# Clone drush packages.
+if [ ! -d drush ]; then
+    mkdir drush
+    cd drush
+    git clone git@git.drupal.org:project/provision.git --branch 7.x-3.x
+    git clone git@git.drupal.org:project/registry_rebuild.git --branch 7.x-2.x
+fi
