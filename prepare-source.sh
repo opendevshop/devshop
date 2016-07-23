fb8c80701ea812c0b080f7dde39a42fc4db6b410
Jon Pugh <jon@thinkdrop.net>
2016-07-23 09:38:29 -0400

    Fixing prepare-source.sh script

--- prepare-source.sh
+++ prepare-source.sh
@@ -14,7 +14,7 @@ cd source
 
 # Build a full devshop frontend on the host with drush make, with working-copy option.
 if [ ! -d devmaster-$DEVMASTER_VERSION ]; then
-   drush make ../../build-devmaster.make devmaster-$DEVMASTER_VERSION --working-copy --no-gitinfofile
+   drush make ../build-devmaster.make devmaster-$DEVMASTER_VERSION --working-copy --no-gitinfofile
    cp devmaster-$DEVMASTER_VERSION/sites/default/default.settings.php devmaster-$DEVMASTER_VERSION/sites/default/settings.php
    mkdir devmaster-$DEVMASTER_VERSION/sites/devshop.local.computer
    chmod 777 devmaster-$DEVMASTER_VERSION/sites -R
