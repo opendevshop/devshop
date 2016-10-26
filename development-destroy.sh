30671ac2596b8ebccb8a45583ff40291644ab85d
Jon Pugh <jon@thinkdrop.net>
2016-10-26 17:58:18 -0400

    Destroy all of aegir-home when destroying the environment.

--- development-destroy.sh
+++ development-destroy.sh
@@ -3,7 +3,10 @@
 echo "==========================ÆGIR=========================="
 echo "------------------------ DEVSHOP------------------------"
 echo " Destroying your development containers...              "
-echo " This will eliminate your database. If you do not want this, "
+echo " This will eliminate your database and ALL of your source"
+echo " code at aegir-home."
+echo ""
+echo " If you do not want this, "
 echo " Hit CTRL-C now!"
 echo ""
 echo " Waiting 5 seconds..."
@@ -15,11 +18,10 @@ docker-compose kill
 echo "Æ| docker-compose rm -fv"
 docker-compose rm -fv
 
-echo "Æ| rm -rf aegir-home/.drush"
-rm -rf aegir-home/.drush
+echo "Æ| sudo rm -rf aegir-home"
+sudo rm -rf aegir-home
 
-echo "Æ| rm -rf aegir-home/config"
-rm -rf aegir-home/config
+echo "==========================ÆGIR=========================="
+echo "------------------------ DEVSHOP------------------------"
 
-echo "Æ| sudo rm -rf aegir-home/devmaster-1.x/sites/devshop.local.computer"
-sudo rm -rf aegir-home/devmaster-1.x/sites/devshop.local.computer
+echo " Development environment destroyed.  "
