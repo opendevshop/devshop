02cf9f97c48df425bffae81bce008d73cb990d3a
Jon Pugh <jon@thinkdrop.net>
2016-10-30 10:34:40 -0400

    Handle loading tasks on node tasks tab

--- themes/boots/js/boots.js
+++ themes/boots/js/boots.js
@@ -6,31 +6,31 @@
             $('.environment-wrapper').matchHeight();
         },
     }
-    Drupal.behaviors.timeAgo = {
-        attach: function (context, settings) {
-            $.timeago.settings.refreshMillis = 1000;
-            $.timeago.settings.strings = {
-                prefixAgo: null,
-                prefixFromNow: null,
-                suffixAgo: "ago",
-                suffixFromNow: "from now",
-                inPast: 'any moment now',
-                seconds: "%d sec",
-                minute: "1 min",
-                minutes: "%d min",
-                hour: "1 hr",
-                hours: "%d hrs",
-                day: "1 day",
-                days: "%d days",
-                month: "1 month",
-                months: "%d months",
-                year: "1 year",
-                years: "%d years",
-                wordSeparator: " ",
-                numbers: []
-            }
-
-            $(".timeago").timeago();
-        }
-    }
+    // Drupal.behaviors.timeAgo = {
+    //     attach: function (context, settings) {
+    //         $.timeago.settings.refreshMillis = 1000;
+    //         $.timeago.settings.strings = {
+    //             prefixAgo: null,
+    //             prefixFromNow: null,
+    //             suffixAgo: "ago",
+    //             suffixFromNow: "from now",
+    //             inPast: 'any moment now',
+    //             seconds: "%d sec",
+    //             minute: "1 min",
+    //             minutes: "%d min",
+    //             hour: "1 hr",
+    //             hours: "%d hrs",
+    //             day: "1 day",
+    //             days: "%d days",
+    //             month: "1 month",
+    //             months: "%d months",
+    //             year: "1 year",
+    //             years: "%d years",
+    //             wordSeparator: " ",
+    //             numbers: []
+    //         }
+    //
+    //         $(".timeago").timeago();
+    //     }
+    // }
 })(jQuery);
