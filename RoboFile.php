f4aa868e2b6225848b587148605dd41d08f36979
Jon Pugh <jon@thinkdrop.net>
2017-01-27 16:45:05 -0500

    Add code for prepare:host command.

--- RoboFile.php
+++ RoboFile.php
@@ -11,6 +11,40 @@ class RoboFile extends \Robo\Tasks
    * Check for docker, docker-compose and drush. Install them if they are missing.
    */
   public function prepareHost() {
+    // Check for docker
+    $this->say('Checking for Docker...');
+    if ($this->taskDockerRun('hello-world')->printed(FALSE)->run()->wasSuccessful()) {
+      $this->_exec('docker -v');
+      $this->say('Docker detected.');
+    }
+    else {
+      $this->say('Could not run docker command. Find instructons for installing at https://www.docker.com/products/docker');
+    }
+  
+    // Check for docker-compose
+    $this->say('Checking for docker-compose...');
+    if ($this->_exec('docker-compose -v')->wasSuccessful()) {
+      $this->say('docker-compose detected.');
+    }
+    else {
+      $this->yell('Could not run docker-compose command.', 40, 'red');
+      $this->say("Run the following command as root to install it or see https://docs.docker.com/compose/install/ for more information.");
+      
+      $this->say('curl -L "https://github.com/docker/compose/releases/download/1.10.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && chmod +x /usr/local/bin/docker-compose');
+    }
+    
+    // Check for drush
+    $this->say('Checking for drush...');
+    if ($this->_exec('drush --version')->wasSuccessful()) {
+      $this->say('drush detected.');
+    }
+    else {
+      $this->yell('Could not run drush.', 40, 'red');
+      $this->say("Run the following command as root to install it or see http://www.drush.org/en/master/install/ for more information.");
+    
+      $this->say('php -r "readfile(\'https://s3.amazonaws.com/files.drush.org/drush.phar\');" > /usr/local/bin/drush
+ && chmod +x /usr/local/bin/drush');
+    }
   }
   
   /**
