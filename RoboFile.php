b47f1343bc6eb989a738029a2f18ce0e8fa23781
Jon Pugh <jon@thinkdrop.net>
2017-04-19 18:45:31 -0400

    Missing option in docker run.

--- RoboFile.php
+++ RoboFile.php
@@ -229,7 +229,7 @@ class RoboFile extends \Robo\Tasks
       
       // Launch a devmaster container as if it were the last release, then run hostmaster-migrate on it, then run the tests.
       // @TODO: Instead of run-tests.sh, run a test-upgrade.sh script to run hostmaster-migrate, then run-tests.sh.
-      $cmd = "docker-compose run -e UPGRADE_FROM_VERSION={$version} -e AEGIR_HOSTMASTER_ROOT=/var/aegir/devmaster-{$version} AEGIR_HOSTMASTER_ROOT_TARGET=$root_target -e AEGIR_VERSION={$version} -e AEGIR_MAKEFILE=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/build-devmaster.make devmaster '/usr/share/devshop/tests/test-upgrade.sh' ";
+      $cmd = "docker-compose run -e UPGRADE_FROM_VERSION={$version} -e AEGIR_HOSTMASTER_ROOT=/var/aegir/devmaster-{$version} -e AEGIR_HOSTMASTER_ROOT_TARGET=$root_target -e AEGIR_VERSION={$version} -e AEGIR_MAKEFILE=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/build-devmaster.make devmaster '/usr/share/devshop/tests/test-upgrade.sh' ";
     }
     else {
       $cmd = "docker-compose up -d";
