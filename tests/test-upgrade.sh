2f775c5dbe1ab0507804d736af56e385783b5277
Jon Pugh <jon@thinkdrop.net>
2017-04-20 12:00:18 -0400

    Revert "Wrong tests folder, for now."
    This reverts commit 8c8ad6f6dce72b0782e00b67d1c61b0b1018a135.

--- tests/test-upgrade.sh
+++ tests/test-upgrade.sh
@@ -6,9 +6,7 @@ drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y
 drush @hostmaster vset hosting_queued_paused 1
 
 # Run the test suite.
-#cd ${AEGIR_HOSTMASTER_ROOT_TARGET}/profiles/devmaster/tests
-
-cd /usr/share/devshop/tests
+cd ${AEGIR_HOSTMASTER_ROOT_TARGET}/profiles/devmaster/tests
 
 composer install
 bin/behat --profile=devmaster
