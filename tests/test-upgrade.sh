8c8ad6f6dce72b0782e00b67d1c61b0b1018a135
Jon Pugh <jon@thinkdrop.net>
2017-04-20 08:00:10 -0400

    Wrong tests folder, for now.

--- tests/test-upgrade.sh
+++ tests/test-upgrade.sh
@@ -6,7 +6,9 @@ drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y
 drush @hostmaster vset hosting_queued_paused 1
 
 # Run the test suite.
-cd ${AEGIR_HOSTMASTER_ROOT_TARGET}/profiles/devmaster/tests
+#cd ${AEGIR_HOSTMASTER_ROOT_TARGET}/profiles/devmaster/tests
+
+cd /usr/share/devshop/tests
 
 composer install
 bin/behat --profile=devmaster
