013594effd996de0c79d0443b04a99c2dc2a7f2a
Jon Pugh <jon@thinkdrop.net>
2017-06-23 14:41:54 -0400

    Use comment character to try to avoid being run.

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -3,32 +3,32 @@
 set -e
 
 # Run remaining tasks from install process.
-echo ">> Running remaining tasks: drush @hostmaster hosting-tasks --fork=0 --strict=0"
+echo "# Running remaining tasks: drush @hostmaster hosting-tasks --fork=0 --strict=0"
 drush @hostmaster hosting-tasks --fork=0 --strict=0
 
-echo ">> Running remaining tasks: Complete!"
+echo "# Running remaining tasks: Complete!"
 
 if [[ $* == *--upgrade* ]]; then
-      echo ">> Triggering Upgrade: Running drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y"
+      echo "# Triggering Upgrade: Running drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y"
 
       # Force all tasks to appear as completed.'
       drush @hostmaster sql-query "UPDATE hosting_task SET task_status = 1;"
 
       drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y
 
-      echo ">> Upgrade Complete."
+      echo "# Upgrade Complete."
 fi
 
 #set -ex
 
 # Pause the task queue.
-echo ">> Running 'drush @hostmaster vset hosting_queued_paused 1'..."
+echo "# Running 'drush @hostmaster vset hosting_queued_paused 1'..."
 drush @hostmaster vset hosting_queued_paused 1
 
 # Run the test suite.
-echo ">> Running '/usr/share/devshop/bin/devshop devmaster:test'..."
+echo "# Running '/usr/share/devshop/bin/devshop devmaster:test'..."
 /usr/share/devshop/bin/devshop devmaster:test
 
 # Unpause the task queue.
-echo ">> Running 'drush @hostmaster vset hosting_queued_paused 0'..."
+echo "# Running 'drush @hostmaster vset hosting_queued_paused 0'..."
 drush @hostmaster vset hosting_queued_paused 0
