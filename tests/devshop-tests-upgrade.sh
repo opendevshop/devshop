1548a4ae3a94996bb2b1b0699341cb2ca47743da
Jon Pugh <jon@thinkdrop.net>
2017-07-06 15:40:22 -0400

    Try spawing hosting-queued in the background so tasks are run during hosting-migrate.

--- tests/devshop-tests-upgrade.sh
+++ tests/devshop-tests-upgrade.sh
@@ -11,13 +11,20 @@ echo ">> Running remaining tasks: Complete!"
 echo ">> Triggering Upgrade: Running drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y"
 
 # Force all tasks to appear as completed.'
-echo ">> Resetting all tasks..."
-drush @hostmaster sql-query "SELECT * FROM hosting_task;"
-drush @hostmaster sql-query "UPDATE hosting_task SET task_status = 1;"
-drush @hostmaster sql-query "SELECT * FROM hosting_task;"
+#echo ">> Resetting all tasks..."
+#drush @hostmaster sql-query "SELECT * FROM hosting_task;"
+#drush @hostmaster sql-query "UPDATE hosting_task SET task_status = 1;"
+#drush @hostmaster sql-query "SELECT * FROM hosting_task;"
+
+
+echo ">> Spawing hosting queued in a new process so tasks run during upgrade/migrate..."
+drush @hostmaster en hosting_queued -y
+drush @hostmaster hosting-queued &
 
 drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y -v
 
 echo ">> Upgrade Complete."
+
+drush @hostmaster dis hosting_queued -y
 
 bash /usr/share/devshop/tests/devshop-tests.sh
