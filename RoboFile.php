ec518b06ebf95f6816133d9199909eb9b87308e1
Jon Pugh <jon@thinkdrop.net>
2017-07-06 13:32:17 -0400

    Adding some debugging, need to see what's in the folder.

--- RoboFile.php
+++ RoboFile.php
@@ -216,7 +216,7 @@ class RoboFile extends \Robo\Tasks
       }
     }
 
-    $command = "bash /usr/share/devshop/tests/devshop-tests.sh";
+    $command = "ls -la /usr/share/devshop/tests && bash /usr/share/devshop/tests/devshop-tests.sh";
     
     if ($opts['test']) {
       $cmd = "docker-compose run -T -e BEHAT_PATH={$_SERVER['BEHAT_PATH']} -e TERM=xterm devmaster '$command'";
