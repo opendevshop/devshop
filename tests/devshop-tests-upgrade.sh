ec241ed7ddf205b081b3597281f93950ff3841b8
Jon Pugh <jon@thinkdrop.net>
2017-07-06 15:20:19 -0400

    Print out hosting_task table for debugging.

--- tests/devshop-tests-upgrade.sh
+++ tests/devshop-tests-upgrade.sh
@@ -11,6 +11,8 @@ echo ">> Running remaining tasks: Complete!"
 echo ">> Triggering Upgrade: Running drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y"
 
 # Force all tasks to appear as completed.'
+echo ">> Resetting all tasks..."
+drush @hostmaster sql-query "SELECT * FROM hosting_task;"
 drush @hostmaster sql-query "UPDATE hosting_task SET task_status = 1;"
 
 drush @hostmaster hostmaster-migrate $HOSTNAME $AEGIR_HOSTMASTER_ROOT_TARGET -y -v
