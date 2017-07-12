101e0f3f535e1da55eedd92a674490dae59847b2
Jon Pugh <jon@thinkdrop.net>
2017-07-12 10:04:36 -0400

    Use proper volume syntax in robo up --mode=install.sh

--- RoboFile.php
+++ RoboFile.php
@@ -295,7 +295,7 @@ class RoboFile extends \Robo\Tasks
       if (!$this->taskDockerRun($opts['install-sh-image'])
         ->name('devshop_container')
         ->volume(__DIR__, '/usr/share/devshop')
-        ->volume('aegir-home', '/var/aegir')
+        ->volume(__DIR__ . '/aegir-home', '/var/aegir')
         ->option('--hostname', 'devshop.local.computer')
         ->option('--add-host', '"' . $_SERVER['SITE_HOSTS'] . '":127.0.0.1')
         ->option('--volume', '/sys/fs/cgroup:/sys/fs/cgroup:ro')
