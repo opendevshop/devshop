cc27876344dc67e864f1bb0abe11b13112fa119e
Jon Pugh <jon@thinkdrop.net>
2017-12-19 12:08:17 -0500

    Fix Project aliases so they are written before every task.

--- modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
+++ modules/devshop/devshop_projects/drush/Provision/Config/project_aliases.tpl.php
@@ -3,22 +3,22 @@
  * @file
  * Template file for a project's alias file.
  */
-print "<?php // Automatically written by devshop when a project is verified. Do Not Edit. \n";
+print "<?php // Automatically written by devshop. Do Not Edit. \n";
 
-foreach ($project['environments'] as $name => $environment) {
+foreach ($project->environments as $name => $environment) {
 
-  if ($environment['site_status'] != 1) {
+  if ($environment->site_status != 1) {
     continue;
   }
   // Tell drush to inherit from the provision site alias record.
   $alias = array(
-    'parent' => '@' . $environment['uri']
+    'parent' => '@' . $environment->uri
   );
 
   // If web server is not server master, add "remote host and user.
-  if (d($environment['drush_alias'])->platform->web_server->name != '@server_master') {
-    $alias['remote-host'] = d($environment['drush_alias'])->platform->web_server->remote_host;
-    $alias['remote-user'] = d($environment['drush_alias'])->platform->web_server->script_user;
+  if (d($environment->drush_alias)->platform->web_server->name != '@server_master') {
+    $alias['remote-host'] = d($environment->drush_alias)->platform->web_server->remote_host;
+    $alias['remote-user'] = d($environment->drush_alias)->platform->web_server->script_user;
   }
 
   $export = var_export($alias, TRUE);
@@ -28,7 +28,7 @@ $aliases['<?php print $name; ?>'] = <?php print $export; ?>;
 
   <?php
 }
-foreach ($project['settings']['aliases'] as $name => $remote_alias) {
+foreach ($project->settings->aliases as $name => $remote_alias) {
   $export = var_export($remote_alias, TRUE);
   ?>
 
