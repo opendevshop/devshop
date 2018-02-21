19b57f1ab13eee178c4d21c3183468b978b57ba2
Jon Pugh <jon@thinkdrop.net>
2018-02-21 14:08:55 -0500

    Fixing documentation in install.sh

--- install.sh
+++ install.sh
@@ -5,21 +5,36 @@
 #
 #  This script will install a full devshop server from scratch. 
 #  
-#  Please read the full "Installing DevShop" instructions at https://devshop.readthedocs.org/en/latest/install/
+#  Please read the full "Installing DevShop" instructions at https://docs.opendevshop.com/install.html
 #
 #  Before you start, please visit https://github.com/opendevshop/devshop/releases to be sure you have the latest version of this script,
-#  Or you may try the 0.x script with the URL https://raw.githubusercontent.com/opendevshop/devshop/0.x/install.sh
-# 
-#  Must run with root or sudo privileges:
+#
+#  DNS & Hostnames
+#  ---------------
+#  For devshop to work, the server's hostname must be a fullly qualified domain name that resolves to an accessible IP address.
+#
+#  Before you run this script, add DNS records for this server:
+#
+#    devshop.mydomain.com. 1800 IN A 1.2.3.4
+#    *.devshop.mydomain.com. 1800 IN A 1.2.3.4
+#
+#  This install script will attempt to set your hostname for you, if you use the --hostname option.
+#
+#
+#  Running Install.sh
+#  ==================
+#
+#   Must run as root:
 #  
-#    ubuntu@devshop:~$ wget https://raw.githubusercontent.com/opendevshop/devshop/0.x/install.sh
-#    ubuntu@devshop:~$ bash install.sh
+#    root@ubunu:~# wget https://raw.githubusercontent.com/opendevshop/devshop/1.0.0-rc3/install.sh
+#    root@ubunu:~# bash install.sh --hostname=devshop.mydomain.com
 #
 #  Options:
 #    --hostname           The desired fully qualified domain name to set as this machine's hostname
 #    --server-webserver   Set to 'nginx' if you want to use that as your webserver instead of apache.
 #    --makefile           The makefile to use to build the front-end site.
 #    --playbook           The Ansible playbook.yml file to use other than the included playbook.yml.
+#
 
 # Version used for cloning devshop playbooks
 # Must be a branch or tag.
