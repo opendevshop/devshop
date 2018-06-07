cb566b67ed88f2a54eef4d67aa6a1170a61044fb
Jon Pugh <jon@thinkdrop.net>
2018-06-07 17:16:12 -0400

    Automated Commit from DevShop Robofile.php `release` command. Preparing version 1.3.2.

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@
 
 # Version used for cloning devshop playbooks
 # Must be a branch or tag.
-DEVSHOP_VERSION=1.x
+DEVSHOP_VERSION=1.3.2
 SERVER_WEBSERVER=apache
 MAKEFILE_PATH=''
 AEGIR_USER_UID=${AEGIR_USER_UID:-1000}
