03a1a74e335800cf98111d987b1e461f09cf7ace
Jon Pugh <jon@thinkdrop.net>
2018-07-19 13:57:38 -0400

    Set hostname to devshop.local.computer

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -3,5 +3,6 @@
 # Use --user and --entrypoint to skip the entry script and just run bash as root.
 # docker run --rm --name devshop-server --hostname devshop.server.local.computer -ti --entrypoint bash --user=root devshop/server:local
 
-docker run --rm --name devshop-server --hostname devshop.server.local.computer -t -i -e MYSQL_ROOT_PASSWORD=securepass -v devshop-projects:/var/aegir/projects -p 80:80 -d devshop/server:local
+docker run --rm --name devshop-server --hostname devshop.local.computer -t -i -e MYSQL_ROOT_PASSWORD=securepass -v devshop-projects:/var/aegir/projects -p 80:80 -d devshop/server:local
+
 docker logs -f devshop-server
