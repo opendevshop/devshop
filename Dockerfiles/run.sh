5500007e87b77b09863a60fe34f45b19621af1a7
Jon Pugh <jon@thinkdrop.net>
2018-07-19 13:57:14 -0400

    Create the projects directory in the docckerfile, and rearrange the order of the drush make command.

--- Dockerfiles/run.sh
+++ Dockerfiles/run.sh
@@ -3,5 +3,5 @@
 # Use --user and --entrypoint to skip the entry script and just run bash as root.
 # docker run --rm --name devshop-server --hostname devshop.server.local.computer -ti --entrypoint bash --user=root devshop/server:local
 
-docker run --rm --name devshop-server --hostname devshop.server.local.computer -t -i -e MYSQL_ROOT_PASSWORD=securepass -v devshop-server:/var/aegir -p 80:80 -d devshop/server:local
+docker run --rm --name devshop-server --hostname devshop.server.local.computer -t -i -e MYSQL_ROOT_PASSWORD=securepass -v devshop-projects:/var/aegir/projects -p 80:80 -d devshop/server:local
 docker logs -f devshop-server
