8c0db5b54ac36ab6b5c7f5d693a40bf54ecbdf91
Jon Pugh <jon@thinkdrop.net>
2018-11-17 11:43:29 -0500

    Switch to 1.x-dev branch in build-devmaster.make, and set to git, so it's always up to date. Update robofile release command to handle this.

--- RoboFile.php
+++ RoboFile.php
@@ -665,7 +665,7 @@ class RoboFile extends \Robo\Tasks {
     }
 
     if ($this->confirm("Write '$drupal_org_version' to build-devmaster.make? ")) {
-      $this->_exec("sed -i -e 's/projects\[devmaster\]\[version\] = 1.x/projects[devmaster][version] = $drupal_org_version/' build-devmaster.make");
+      $this->_exec("sed -i -e 's/projects\[devmaster\]\[version\] = 1.x-dev/projects[devmaster][version] = $drupal_org_version/' build-devmaster.make");
     }
 
     if ($this->confirm("Write '$drupal_org_version' to drupal-org.make for devshop_stats? ")) {
