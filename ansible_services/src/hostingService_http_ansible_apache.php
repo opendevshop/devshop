ed97dfe15c1285f93ba494c96dce2386c927ee3e
Jon Pugh <jon@thinkdrop.net>
2018-11-20 16:09:41 -0500

    Update default role versions for php and mysql.

--- ansible_services/src/hostingService_http_ansible_apache.php
+++ ansible_services/src/hostingService_http_ansible_apache.php
@@ -46,11 +46,11 @@ class hostingService_http_ansible_apache extends hostingService_http_https_apach
             'opendevshop.aegir-apache',
             'geerlingguy.php' => array(
                 'name' => 'geerlingguy.php',
-                'version' => '2.0.3',
+                'version' => '3.5.0',
             ),
             'geerlingguy.php-mysql' => array(
               'name' => 'geerlingguy.php-mysql',
-              'version' => '1.2.0',
+              'version' => '2.0.1',
             ),
             'geerlingguy.composer'
         );
