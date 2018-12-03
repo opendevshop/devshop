3a1c6572b3458cfcdfef830321862f3cfab5e300
Jon Pugh <jon@thinkdrop.net>
2018-12-03 14:48:03 -0500

    Adding new login/forgot password templates and tests to ensure turning on the license turns on the messaging.

--- themes/boots/page--user--reset.tpl.php
+++ themes/boots/page--user--reset.tpl.php
@@ -10,19 +10,19 @@
     <div class="row">
         <div class="col-sm-12 col-md-12 col-lg-12 page-header login-header">
             <h1><?php print t('Hi!'); ?>
-                <small><?php print t('Welcome to @hostname', array(
-                        '@hostname' => $_SERVER['HTTP_HOST']
-                  )) ?></small>
+                <small><?php print t('Welcome to DevShop.Support') ?></small>
             </h1>
         </div>
     </div>
     <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-12 p">
+        <div class="col-sm-12 col-md-12 col-lg-12 ">
 
             <div id="auth_box" class="login row">
-                <h2 class="title"><?php print t('Sign In'); ?>
-                </h2>
-
+                <h2 class="title"><?php print t('Reset Password'); ?><small class="pull-right"><?php print l(t('Sign In'), 'user/login', array(
+                      'attributes' => array(
+                        'class' => array('btn btn-default')
+                      )
+                    )); ?></small></h2>
               <?php print $messages; ?>
 
               <?php print render($page['content']); ?>
