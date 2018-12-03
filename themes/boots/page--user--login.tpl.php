958ac945e4bace97e6dda70e3c7853e9f2109bb1
Jon Pugh <jon@thinkdrop.net>
2018-12-03 18:20:08 -0500

    Show both logos!

--- themes/boots/page--user--login.tpl.php
+++ themes/boots/page--user--login.tpl.php
@@ -16,8 +16,18 @@
             </h1>
         </div>
     </div>
+    <div class="row logos">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <img src="/<?php print path_to_theme(); ?>/devshop.support.png" class="img-responsive login-images">
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <a href="<?php print url('<front>'); ?>">
+                <img src="<?php print $logo; ?>" alt="<?php print $site_name; ?>" class="img-responsive login-images">
+            </a>
+        </div>
+    </div>
     <div class="row">
-        <div class="col-sm-12 col-md-12 col-lg-12 p">
+        <div class="col-sm-12 col-md-12 col-lg-12">
 
             <div id="auth_box" class="login row">
                 <h2 class="title"><?php print t('Sign In'); ?>
