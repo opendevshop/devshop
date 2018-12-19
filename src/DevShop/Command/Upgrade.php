fa282caaeba6ba7afe44ef9029196be8e0c1faa6
Jon Pugh <jon@thinkdrop.net>
2018-12-19 11:53:04 -0500

    Comment all the stuff out of devshop upgrade command now that all the logic is in the playbooks and the devmaster:upgrade command. devshop upgrade will now simply run self-update (which updates the CLI and the ansible roles) and then `devshop verify:system` which runs the playbook.yml with the new roles!

--- src/DevShop/Command/Upgrade.php
+++ src/DevShop/Command/Upgrade.php
@@ -59,14 +59,13 @@ class Upgrade extends Command
       throw new \Exception('aegir user does not exist! DevShop is not installed! Installation aborted.');
 
     }
-    $output->writeln('');
 
     // Check current user is root
     $pwu_data = posix_getpwuid(posix_geteuid());
     if ($pwu_data['name'] != 'root') {
       throw new \Exception('You must run this command as the root user. Run "sudo devshop upgrade" to run as root. Upgrade aborted.');
     }
-    $output->writeln('');
+
     $current_version = $this->getApplication()->getVersion();
 
     // Look for an active devmaster
@@ -76,40 +75,41 @@ class Upgrade extends Command
     if (empty($devmaster_root) || !file_exists($devmaster_root)) {
       throw new \Exception('Unable to find a devmaster.  The drush @hostmaster alias does not exist.  Unable to upgrade.');
     }
-
-    // Lookup latest version.
-    $output->writeln('Checking for latest releases...');
-    $client = new \Github\Client();
-    $release = $client->repositories()->releases()->latest('opendevshop', 'devshop');
-
-    // Make sure we got the release info
-    if (empty($release)) {
-      $output->writeln("<fg=red>Unable to retrieve releases from GitHub.  Try again later, or specify a release.</>");
-      $latest_release = '0.x';
-      $output->writeln("Assuming development version <info>$latest_release</info>.");
-    }
-    else {
-      $latest_release = $release['tag_name'];
-      $output->writeln("<info>Latest Version</info> $latest_release");
-    }
-
-    $default_version = $input->getArgument('devshop-version')? $input->getArgument('devshop-version'): $latest_release;
-    $target_version = '';
-
-    // Warn if default is not latest
-    if ($latest_release != $default_version) {
-        $output->writeln("<fg=red>WARNING:</> You have specified a release that is not the latest.");
-    }
-
-    // Confirm version
-    while ($this->checkVersion($target_version) == FALSE) {
-      $question = new Question("Target Version: (Default: $default_version) ", $default_version);
-      $target_version = $helper->ask($input, $output, $question);
-
-      if (!$this->checkVersion($target_version)) {
-        $output->writeln("<fg=red>Version $target_version not found</>");
-      }
-    }
+//
+//    // Lookup latest version.
+//    $output->writeln('Checking for latest releases...');
+//    $client = new \Github\Client();
+//    $release = $client->repositories()->releases()->latest('opendevshop', 'devshop');
+//
+//    // Make sure we got the release info
+//    if (empty($release)) {
+//      $output->writeln("<fg=red>Unable to retrieve releases from GitHub.  Try again later, or specify a release.</>");
+//      $latest_release = '0.x';
+//      $output->writeln("Assuming development version <info>$latest_release</info>.");
+//    }
+//    else {
+//      $latest_release = $release['tag_name'];
+//      $output->writeln("<info>Latest Version</info> $latest_release");
+//    }
+//
+//    $default_version = $input->getArgument('devshop-version')? $input->getArgument('devshop-version'): $latest_release;
+//    $target_version = '';
+//
+//    // Warn if default is not latest
+//    if ($latest_release != $default_version) {
+//        $output->writeln("<fg=red>WARNING:</> You have specified a release that is not the latest.");
+//    }
+//
+//    // Confirm version
+//    while ($this->checkVersion($target_version) == FALSE) {
+//      $question = new Question("Target Version: (Default: $default_version) ", $default_version);
+//      $target_version = $helper->ask($input, $output, $question);
+//
+//      if (!$this->checkVersion($target_version)) {
+//        $output->writeln("<fg=red>Version $target_version not found</>");
+//      }
+//    }
+    $target_version = $input->getArgument('devshop-version');
     $output->writeln("Version $target_version confirmed.");
 
     $devmaster_makefile = $input->getOption('makefile');
@@ -137,25 +137,54 @@ class Upgrade extends Command
       $target_path = "/var/aegir/devmaster-{$target_version}-{$variant}";
     }
 
+    /**
+     * Upgrade Path:
+     *   1. self-update
+     *   2. exec('devshop verify:system')
+     *
+     *      Everything else is run by the playbooks, including `devshop devmaster:upgrade`
+     */
+    $output->writeln('Running devshop self-update...');
+    $command = $this->getApplication()->find('self-update');
+    $arguments = array(
+      'command' => 'self-update',
+      'devshop-version' => $target_version,
+      '--no-interaction' => $input->getOption('no-interaction'),
+    );
+    $commandInput = new ArrayInput($arguments);
     $output->writeln('');
-
-    $output->writeln('UPGRADE OPTIONS');
-    $output->writeln("<info>Current CLI Version: </info>       $current_version");
-    $output->writeln("<info>Current DevMaster Version: </info> $devmaster_version");
-    $output->writeln("<info>Current DevMaster Path: </info>    $devmaster_root");
-    $output->writeln("<info>Current DevMaster Site: </info>    $devmaster_uri");
-    $output->writeln('');
-
-    $output->writeln("<info>Target Version: </info> " . $target_version);
-    $output->writeln("<info>Target DevMaster Path: </info> " . $target_path);
-    $output->writeln("<info>Target DevMaster Makefile: </info> " . $devmaster_makefile);
-
-    $output->writeln('');
-
-    // Check for site in target path
-    if (file_exists($target_path)) {
-      throw new \Exception("There is already a site located at <comment>$target_path</comment>. Please check your version and paths and try again.");
+    if ($command->run($commandInput, $output) != 0) {
+      throw new \Exception('The command self-update failed.');
     }
+
+    // Run devshop verify:system in a new process so it picks up on the updated devshop CLI and ansible roles.
+    $output->writeln('Running devshop verify-system...');
+    $path_to_devshop_bin = dirname(dirname(dirname(__DIR__))) . '/bin/devshop';
+    $process = new Process("$path_to_devshop_bin verify:system");
+    $process->setTimeout(NULL);
+    $process->run(function ($type, $buffer) {
+      echo $buffer;
+    });
+
+//    $output->writeln('');
+//
+//    $output->writeln('UPGRADE OPTIONS');
+//    $output->writeln("<info>Current CLI Version: </info>       $current_version");
+//    $output->writeln("<info>Current DevMaster Version: </info> $devmaster_version");
+//    $output->writeln("<info>Current DevMaster Path: </info>    $devmaster_root");
+//    $output->writeln("<info>Current DevMaster Site: </info>    $devmaster_uri");
+//    $output->writeln('');
+//
+//    $output->writeln("<info>Target Version: </info> " . $target_version);
+//    $output->writeln("<info>Target DevMaster Path: </info> " . $target_path);
+//    $output->writeln("<info>Target DevMaster Makefile: </info> " . $devmaster_makefile);
+//
+//    $output->writeln('');
+//
+//    // Check for site in target path
+//    if (file_exists($target_path)) {
+//      throw new \Exception("There is already a site located at <comment>$target_path</comment>. Please check your version and paths and try again.");
+//    }
 
     //@TODO: Finalize the upgrade process.
     // Aegir's process is split between 'upgrade.sh.txt' and a drush command "hostmaster-migrate"
@@ -169,62 +198,62 @@ class Upgrade extends Command
     // 6. Run "install.sh" as root (should be renamed) to run the ansible playbook on the server.
 
     // 3. Git checkout /usr/share/devshop to get the latest release.
-
-    // Announce devmaster upgrade.
-    $output->writeln('');
-    $output->writeln("<info>Devmaster Upgraded to {$target_version}.</info>");
-
-    // Run the ansible playbook.
-    $output->writeln('');
-    $question = new ConfirmationQuestion("Run playbook (y/n) ", false);
-
-    // If they say no, exit.
-    if ($input->isInteractive() && !$helper->ask($input, $output, $question)) {
-      $output->writeln("<fg=red>Upgrade cancelled.</>");
-      $output->writeln('');
-      return;
-    }
-
-    // If they say yes, run the command.
-    // @TODO: can we remove "install"?? it just calls install.sh and is not used anywhere and is very confusing.
-    $output->writeln('');
-    $command = $this->getApplication()->find('install');
-
-    $arguments = array(
-        'command' => 'install',
-        'devshop-version' => $target_version,
-        '--yes' => 1,
-    );
-
-    $upgradeInput = new ArrayInput($arguments);
-    $output->writeln('');
-
-    if ($command->run($upgradeInput, $output) != 0) {
-      $output->writeln("<fg=red>Playbook run failed!</>");
-      $output->writeln('');
-    }
-
-    $output->writeln("<info>Upgrade completed!  You may use the link above to login or run the command 'devshop login'.</info>");
-
-    // Schedule the command for deletion.
-    $output->writeln('');
-    $question = new ConfirmationQuestion("STEP 3: Schedule deletion of old platform ($devmaster_root) ", false);
-    $cmd = "drush @hostmaster platform-delete $devmaster_root -y";
-
-    $question = new ConfirmationQuestion("Run the command: <comment>$cmd</comment> (y/N) ");
-
-    // If they say no, exit.
-    if ($input->isInteractive() && !$helper->ask($input, $output, $question)) {
-      $output->writeln("<fg=red>Old devmaster platform was not deleted.</> You should find and delete the platform at {$devmaster_root}");
-      $output->writeln('');
-      return;
-    }
-
-    // If they say yes, run drush @hostmaster platform-delete /var/aegir/devmaster-PATH
-    $process = new Process("su aegir - -c '$cmd'");
-    $process->setTimeout(NULL);
-    $process->run(function ($type, $buffer) {
-      echo $buffer;
-    });
+//
+//    // Announce devmaster upgrade.
+//    $output->writeln('');
+//    $output->writeln("<info>Devmaster Upgraded to {$target_version}.</info>");
+//
+//    // Run the ansible playbook.
+//    $output->writeln('');
+//    $question = new ConfirmationQuestion("Run playbook (y/n) ", false);
+//
+//    // If they say no, exit.
+//    if ($input->isInteractive() && !$helper->ask($input, $output, $question)) {
+//      $output->writeln("<fg=red>Upgrade cancelled.</>");
+//      $output->writeln('');
+//      return;
+//    }
+//
+//    // If they say yes, run the command.
+//    // @TODO: can we remove "install"?? it just calls install.sh and is not used anywhere and is very confusing.
+//    $output->writeln('');
+//    $command = $this->getApplication()->find('install');
+//
+//    $arguments = array(
+//        'command' => 'install',
+//        'devshop-version' => $target_version,
+//        '--yes' => 1,
+//    );
+//
+//    $upgradeInput = new ArrayInput($arguments);
+//    $output->writeln('');
+//
+//    if ($command->run($upgradeInput, $output) != 0) {
+//      $output->writeln("<fg=red>Playbook run failed!</>");
+//      $output->writeln('');
+//    }
+//
+//    $output->writeln("<info>Upgrade completed!  You may use the link above to login or run the command 'devshop login'.</info>");
+//
+//    // Schedule the command for deletion.
+//    $output->writeln('');
+//    $question = new ConfirmationQuestion("STEP 3: Schedule deletion of old platform ($devmaster_root) ", false);
+//    $cmd = "drush @hostmaster platform-delete $devmaster_root -y";
+//
+//    $question = new ConfirmationQuestion("Run the command: <comment>$cmd</comment> (y/N) ");
+//
+//    // If they say no, exit.
+//    if ($input->isInteractive() && !$helper->ask($input, $output, $question)) {
+//      $output->writeln("<fg=red>Old devmaster platform was not deleted.</> You should find and delete the platform at {$devmaster_root}");
+//      $output->writeln('');
+//      return;
+//    }
+//
+//    // If they say yes, run drush @hostmaster platform-delete /var/aegir/devmaster-PATH
+//    $process = new Process("su aegir - -c '$cmd'");
+//    $process->setTimeout(NULL);
+//    $process->run(function ($type, $buffer) {
+//      echo $buffer;
+//    });
   }
 }
