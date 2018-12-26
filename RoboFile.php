387071301ca2be33b1fda6b2273a42a696f14c3c
Jon Pugh <jon@thinkdrop.net>
2018-12-26 10:00:56 -0500

    Add commented out makefile and install-path options in preparation for new install-path option.

--- RoboFile.php
+++ RoboFile.php
@@ -536,6 +536,11 @@ class RoboFile extends \Robo\Tasks {
         // Run install.sh old version.
         $version = self::UPGRADE_FROM_VERSION;
         $this->_exec("curl -fsSL https://raw.githubusercontent.com/opendevshop/devshop/{$version}/install.sh -o {$this->devshop_root_path}/install.{$version}.sh");
+
+        // @TODO: Set makefile and devshop install path options once available in install.sh.
+        // $opts['install-sh-options'] .= " --makefile=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/install.sh ";
+        // $opts['install-sh-options'] .= " --install-path=/usr/share/devshop-{$version}";
+
         $this->taskDockerExec('devshop_container')
           ->exec("bash /usr/share/devshop/install.{$version}.sh " . $opts['install-sh-options'])
           ->run();
