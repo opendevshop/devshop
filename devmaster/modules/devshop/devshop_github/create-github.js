67c0f0e06dede4f17f35abf2d929b41b30450c1d
Jon Pugh <jon@thinkdrop.net>
2019-04-03 16:48:01 -0400

    Add ability to Create a new repo. Loads the users allowed github organizations. Use the new repo name for the project code name.

--- devmaster/modules/devshop/devshop_github/create-github.js
+++ devmaster/modules/devshop/devshop_github/create-github.js
@@ -4,15 +4,28 @@
  * Step 1: Takes the entered name and creates a base url and code path from it.
  */
 (function ($) {
+    Drupal.devshopFilterProjectName = function (inputElement) {
+        return inputElement.val().split("/").pop().replace('.git', '').replace(/[^a-z0-9]/gi, '').toLowerCase()
+    }
+
+    Drupal.devshopSetProjectName = function (inputElement) {
+        var fixedProjectName =  Drupal.devshopFilterProjectName(inputElement);
+        if (fixedProjectName != '') {
+            $('#edit-title').val(fixedProjectName);
+        }
+    }
+
     Drupal.behaviors.githubSelect = {
         attach: function (context, settings) {
             $( "#edit-git-url-select").change(function(event) {
+                if ($(this).val() != '_help' && $( "#edit-git-url-select" ).val() != '_custom' ) {
+                    Drupal.devshopSetProjectName($(this));
+                }
+            });
 
-                if ($( "#edit-git-url-select" ).val() != '_help' && $( "#edit-git-url-select" ).val() != '_custom' ) {
-                    var fixedProjectName = $(this).val().split("/").pop().replace('.git', '').replace(/[^a-z0-9]/gi, '').toLowerCase();
-                    if (fixedProjectName != '') {
-                        $('#edit-title').val(fixedProjectName);
-                    }
+            $( "#edit-github-repository-name").keyup(function(event) {
+                if ($( "#edit-git-url-select" ).val() == '_create') {
+                    Drupal.devshopSetProjectName($(this));
                 }
             });
         }
