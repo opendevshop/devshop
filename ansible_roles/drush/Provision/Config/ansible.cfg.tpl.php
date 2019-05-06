0830703d3e5b186245a8d54a8e4d8f751f328a9d
Jon Pugh <jon@thinkdrop.net>
2019-05-06 09:47:05 -0400

    Use unixy output and force_color.

--- ansible_roles/drush/Provision/Config/ansible.cfg.tpl.php
+++ ansible_roles/drush/Provision/Config/ansible.cfg.tpl.php
@@ -1,3 +1,5 @@
 # WRITTEN BY aegir_ansible module.  Do not edit. Edit /etc/ansible/ansible.cfg if you need to reconfigure ansible.
 [defaults]
 roles_path = /var/aegir/roles
+stdout_callback  = unixy
+force_color = True
