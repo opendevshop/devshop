25d4f2f729968a5e31ddde11e8c18c3c294cbaae
Jon Pugh <jon@thinkdrop.net>
2019-05-22 10:03:21 -0400

    Fix class name

--- YamlTestsCommandProvider.php
+++ YamlTestsCommandProvider.php
@@ -7,7 +7,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Composer\Command\BaseCommand;
 
-class CommandProvider implements CommandProviderCapability
+class YamlTestsCommandProvider implements CommandProviderCapability
 {
   public function getCommands()
   {
