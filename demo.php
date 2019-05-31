80ec911d9fabd76eaf8410a6c29321d04d25dba4
Jon Pugh <jon@thinkdrop.net>
2019-05-31 08:11:47 -0400

    Adding a demo.php file

---
+++ demo.php
@@ -0,0 +1,21 @@
+#!/usr/bin/env php
+<?php
+
+// Include autoloader
+include('vendor/autoload.php');
+
+// PowerProcess needs IO.
+$input = new \Symfony\Component\Console\Input\ArgvInput($argv);
+$output = new \Symfony\Component\Console\Output\ConsoleOutput();
+
+// Replace Style with your own to change the output style.
+$io = new ProvisionOps\Tools\Style($input, $output);
+
+// Run any command.
+$command = 'ls -la';
+$process = new ProvisionOps\Tools\PowerProcess($command, $io);
+$process->run();
+
+$command = 'git status';
+$process = new ProvisionOps\Tools\PowerProcess($command, $io);
+$process->run();
