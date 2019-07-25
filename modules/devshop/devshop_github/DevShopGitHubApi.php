3934b3e73c1c2b39ceb7ad9420254e54e6590347
Jon Pugh <jon@thinkdrop.net>
2019-07-25 12:37:14 -0400

    Adding drush command for setting statuses so we can debug DevShopGitHubApi object.

--- modules/devshop/devshop_github/DevShopGitHubApi.php
+++ modules/devshop/devshop_github/DevShopGitHubApi.php
@@ -38,64 +38,71 @@ class DevShopGitHubApi {
    *   made against the environment "git_ref", a branch or tag.
    * @param $log_url
    */
-  static function createDeployment($environment, $state = 'queued', $sha  = NULL, $log_url = NULL) {
+  static function createDeployment($environment, $state = 'queued', $description = NULL, $sha  = NULL, $log_url = NULL) {
 
     $project = $environment->project;
     $environment->dashboard_url = url("node/{$environment->site}", array(
       'absolute' => true
     ));
 
-    $deployment = new stdClass();
+    // If Deployment doesn't already exist, create it.
+    try {
+    $client = devshop_github_client();
 
-    // Git Reference. Use sha if specified.
-    $deployment->ref = $sha? $sha: $environment->git_ref;
+    if (empty($environment->github_pull_request->pull_request_object->deployment)) {
+      $deployment = new stdClass();
 
-    // In GitHub's API, "environment" is just a small string it displays on the pull request:
-    $deployment->environment = $project->name . '.' . $environment->name;
+      // Git Reference. Use sha if specified.
+      $deployment->ref = $sha? $sha: $environment->git_ref;
 
-    $deployment->payload = array(
-      'devshop_site_url' => $environment->dashboard_url,
-      'devmaster_url' => $_SERVER['HTTP_HOST'],
-    );
-    $deployment->description = t('DevShop Deployment');
-    $deployment->required_contexts = array();
+      // In GitHub's API, "environment" is just a small string it displays on the pull request:
+      $deployment->environment = $environment->uri;
 
-    // @TODO: Use the developer preview to get this flag: https://developer.github.com/v3/previews/#enhanced-deployments
-    $deployment->transient_environment = true;
+      $deployment->payload = array(
+        'devshop_site_url' => $environment->dashboard_url,
+        'devmaster_url' => $_SERVER['HTTP_HOST'],
+      );
+      $deployment->description = t('DevShop Deployment');
+      $deployment->required_contexts = array();
 
-    // @TODO: Support deployment notifications for production.
-    $deployment->production_environment = false;
+      // @TODO: Use the developer preview to get this flag: https://developer.github.com/v3/previews/#enhanced-deployments
+      $deployment->transient_environment = true;
+
+      // @TODO: Support deployment notifications for production.
+      $deployment->production_environment = false;
+
+      // Create Deployment
+      $post_url = "/repos/$environment->github_owner/$environment->github_repo/deployments";
+      $deployment_data = json_decode($client->getHttpClient()->post($post_url, array(), json_encode($deployment))->getBody(TRUE));
+
+      watchdog('devshop_github', 'Deployment saved: ' . print_r($deployment_data, 1));
+
+      $environment->github_pull_request->pull_request_object->deployment = $deployment_data;
+
+      print "New Deployment created: " . $deployment_data->id;
+    }
+    else {
+      $deployment_data = $environment->github_pull_request->pull_request_object->deployment;
+      print "Old Deployment loaded: " . $deployment_data->id;}
 
     // Deployment Status
     $deployment_status = new stdClass();
     $deployment_status->state = $state;
     $deployment_status->target_url = 'http://' . $environment->uri;
     $deployment_status->log_url = empty($log_url)? $environment->dashboard_url: $log_url;
-    $deployment_status->description = t('New environment is being created.  Please stand by.');
+    $deployment_status->description = $description;
 
     // @TODO: Use developer preview to get this:
     // https://developer.github.com/v3/previews/#deployment-statuses
     // https://developer.github.com/v3/previews/#enhanced-deployments
-    $deployment_status->environment = $deployment->environment;
+    $deployment_status->environment = $deployment_data->environment;
     $deployment_status->environment_url = $environment->url;
-//    $message = '';
-    try {
-      $client = devshop_github_client();
 
-      // Create Deployment
-      $post_url = "/repos/$environment->github_owner/$environment->github_repo/deployments";
-      $deployment_data = json_decode($client->getHttpClient()->post($post_url, array(), json_encode($deployment))->getBody(TRUE));
+    // Create Deployment Status
+    $post_url = "/repos/{$environment->github_owner}/{$environment->github_repo}/deployments/{$deployment_data->id}/statuses";
+    $deployment_status_data = json_decode($client->getHttpClient()->post($post_url, array(), json_encode($deployment_status))->getBody(TRUE));
 
-      watchdog('devshop_github', 'Deployment saved: ' . json_encode($deployment_data));
-
-      // Save Deployment object to PR
-      self::updateDeploymentId($environment->project_nid, $environment->name, $deployment_data->id);
-
-      // Create Deployment Status
-      $post_url = "/repos/{$environment->github_owner}/{$environment->github_repo}/deployments/{$deployment_data->id}/statuses";
-      $deployment_status_data = $client->getHttpClient()->post($post_url, array(), json_encode($deployment_status));
-
-      watchdog('devshop_github', 'Deployment status saved: ' . $deployment_status_data);
+    watchdog('devshop_github', 'Deployment status saved: ' . print_r($deployment_status_data, 1));
     }
     catch (\Exception $e) {
       watchdog('devshop_github', 'GitHub Error: ' . $e->getMessage());
@@ -123,22 +130,23 @@ class DevShopGitHubApi {
       watchdog('devshop_github', 'GitHub Validation Failed Error: ' . $e->getMessage());
 //      $message .= 'GitHub ValidationFailedException Error: ' . $e->getMessage();
     }
+    devshop_github_save_pr_env_data( $environment->github_pull_request->pull_request_object, $environment);
   }
 
-  /**
-   * Save the deployment ID to the environment.
-   *
-   * @param $project_nid
-   * @param $environment_name
-   * @param $deployment_id
-   */
-  public static function updateDeploymentId($project_nid, $environment_name, $deployment_id)  {
-    db_update('hosting_devshop_github_pull_requests', 'pr')
-      ->condition('pr.project_nid', $project_nid)
-      ->condition('pr.environment_name', $environment_name)
-      ->fields(array(
-        'last_deployment_id' => $deployment_id,
-      ))
-      ->execute();
-  }
+//  /**
+//   * Save the deployment ID to the environment.
+//   *
+//   * @param $project_nid
+//   * @param $environment_name
+//   * @param $deployment_id
+//   */
+//  public static function updateDeploymentId($project_nid, $environment_name, $deployment_id)  {
+//    db_update('hosting_devshop_github_pull_requests', 'pr')
+//      ->condition('pr.project_nid', $project_nid)
+//      ->condition('pr.environment_name', $environment_name)
+//      ->fields(array(
+//        'last_deployment_id' => $deployment_id,
+//      ))
+//      ->execute();
+//  }
 }
