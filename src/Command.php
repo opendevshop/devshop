75b18969645d9547ae93541871e90c5810f20e8a
Jon Pugh <jon@thinkdrop.net>
2019-08-05 11:33:28 -0400

    PHPCBF

--- src/Command.php
+++ src/Command.php
@@ -14,15 +14,15 @@ use TQ\Git\Repository\Repository;
 
 // @TODO: Figure out why our plugin isn't properly autoloading.
 if (file_exists(__DIR__.'/../../../../vendor/autoload.php')) {
-  $autoloaderPath = __DIR__.'/../../../../vendor/autoload.php';
+    $autoloaderPath = __DIR__.'/../../../../vendor/autoload.php';
 } elseif (file_exists(__DIR__.'/vendor/autoload.php')) {
-  $autoloaderPath = __DIR__.'/vendor/autoload.php';
+    $autoloaderPath = __DIR__.'/vendor/autoload.php';
 } elseif (file_exists(__DIR__.'/../../autoload.php')) {
-  $autoloaderPath = __DIR__ . '/../../autoload.php';
+    $autoloaderPath = __DIR__ . '/../../autoload.php';
 } if (file_exists(__DIR__.'/../vendor/autoload.php')) {
-  $autoloaderPath = __DIR__ . '/../vendor/autoload.php';
+    $autoloaderPath = __DIR__ . '/../vendor/autoload.php';
 } else {
-  die("CouldXXXX not find autoloader. Run 'composer install'." . __DIR__);
+    die("CouldXXXX not find autoloader. Run 'composer install'." . __DIR__);
 }
 require_once $autoloaderPath;
 
