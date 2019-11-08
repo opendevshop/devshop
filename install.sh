fd623c0a4b91089da13a0fccedb90e23d2e994a2
Jon Pugh <jon@thinkdrop.net>
2019-11-08 12:16:47 -0500

    Add devshop_github_token to install.sh.

--- install.sh
+++ install.sh
@@ -416,6 +416,7 @@ ANSIBLE_EXTRA_VARS+=("playbook_path: ${DEVSHOP_INSTALL_PATH}")
 ANSIBLE_EXTRA_VARS+=("aegir_server_webserver: ${SERVER_WEBSERVER}")
 ANSIBLE_EXTRA_VARS+=("devshop_version: ${DEVSHOP_VERSION}")
 ANSIBLE_EXTRA_VARS+=("aegir_user_uid: ${AEGIR_USER_UID}")
+ANSIBLE_EXTRA_VARS+=("devshop_github_token: ${GITHUB_TOKEN}")
 ANSIBLE_EXTRA_VARS+=("travis: false")
 ANSIBLE_EXTRA_VARS+=("supervisor_running: true")
 
