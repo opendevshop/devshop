b320643b0da27a5dcbb454db1059f7d8c9e309be
Jon Pugh <jon@thinkdrop.net>
2019-11-08 12:35:02 -0500

    Add message that the token was saved.

--- tests/devshop-tests.sh
+++ tests/devshop-tests.sh
@@ -28,6 +28,7 @@ drush @hostmaster en dblog -y
 # Save GitHub Token
 if [ -n "${GITHUB_TOKEN}" ]; then
   drush @hostmaster vset devshop_github_token ${GITHUB_TOKEN}
+  echo ">> Drupal variable set from GITHUB_TOKEN environment variable."
 else
   echo ">> GITHUB_TOKEN environment variable not found."
 fi
