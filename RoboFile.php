3e5fb71cc2881f31ecf3fe4155b9b6102dbcf9fd
Jon Pugh <jon@thinkdrop.net>
2019-12-02 20:23:29 -0500

    Checkout provision to the 7.x-3.x-devshop branch on robo prepare:sourcecode.

--- RoboFile.php
+++ RoboFile.php
@@ -158,6 +158,14 @@ class RoboFile extends \Robo\Tasks {
           ->cloneRepo($url, $this->devshop_root_path . '/' . $path)
           ->run();
       }
+
+      // Checkout provision to the 7.x-3.x-devshop branch.
+      if ($path == 'provision') {
+        $this->taskGitStack()
+          ->dir($this->devshop_root_path . '/' . $path)
+          ->checkout('7.x-3.x-devshop')
+          ->run();
+      }
     }
 
     // @TODO: Detect and clone the right version. This will not be necessary in Ansible 2.6.
