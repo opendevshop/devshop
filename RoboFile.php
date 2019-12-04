0cbe6df20458a267a6dc6fa8ab045caf55b8498e
Jon Pugh <jon@thinkdrop.net>
2019-12-04 13:23:35 -0500

    Make cmd an array.

--- RoboFile.php
+++ RoboFile.php
@@ -386,7 +386,7 @@ class RoboFile extends \Robo\Tasks {
 
       if ($opts['test']) {
         $command = "/usr/share/devshop/tests/devshop-tests.sh";
-        $cmd = "docker-compose -f docker-compose-tests.yml run -T $env devshop '$command'";
+        $cmd[] = "docker-compose -f docker-compose-tests.yml run -T $env devshop '$command'";
       }
       elseif ($opts['test-upgrade']) {
         $version = self::UPGRADE_FROM_VERSION;
@@ -399,28 +399,28 @@ class RoboFile extends \Robo\Tasks {
         //      $cmd = "docker-compose run -e UPGRADE_FROM_VERSION={$version} -e UPGRADE_TO_MAKEFILE= -e AEGIR_HOSTMASTER_ROOT=/var/aegir/devmaster-{$version} -e AEGIR_VERSION={$version} -e AEGIR_MAKEFILE=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/build-devmaster.make -e TRAVIS_BRANCH={$_SERVER['TRAVIS_BRANCH']}  -e TRAVIS_REPO_SLUG={$_SERVER['TRAVIS_REPO_SLUG']} -e TRAVIS_PULL_REQUEST_BRANCH={$_SERVER['TRAVIS_PULL_REQUEST_BRANCH']} devmaster 'run-tests.sh' ";
 
         // Launch a devmaster container as if it were the last release, then run hostmaster-migrate on it, then run the tests.
-        $cmd = "docker-compose -f {$this->devshop_root_path}/docker-compose-tests.yml run $env -e UPGRADE_FROM_VERSION={$version} -e AEGIR_HOSTMASTER_ROOT=/var/aegir/devmaster-{$version} -e AEGIR_HOSTMASTER_ROOT_TARGET=$root_target -e AEGIR_VERSION={$version} -e AEGIR_MAKEFILE=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/build-devmaster.make -e PROVISION_VERSION={$provision_version} devshop '$command'";
+        $cmd[] = "docker-compose -f {$this->devshop_root_path}/docker-compose-tests.yml run $env -e UPGRADE_FROM_VERSION={$version} -e AEGIR_HOSTMASTER_ROOT=/var/aegir/devmaster-{$version} -e AEGIR_HOSTMASTER_ROOT_TARGET=$root_target -e AEGIR_VERSION={$version} -e AEGIR_MAKEFILE=https://raw.githubusercontent.com/opendevshop/devshop/{$version}/build-devmaster.make -e PROVISION_VERSION={$provision_version} devshop '$command'";
       }
       else {
-        $cmd = "docker-compose up -d";
+        $cmd[] = "docker-compose up -d";
 
         # Run final playbook to install devshop.
-        $cmd .= "; docker-compose exec devshop ansible-playbook /usr/share/devshop/docker/playbook.server.yml --tags install-devmaster --extra-vars \"devmaster_skip_install=false\"
+        $cmd[]= " docker-compose exec devshop ansible-playbook /usr/share/devshop/docker/playbook.server.yml --tags install-devmaster --extra-vars \"devmaster_skip_install=false\"
 ";
 
         if ($opts['follow']) {
-          $cmd .= "; docker-compose logs -f";
+          $cmd[] = "docker-compose logs -f";
         }
       }
 
-      if (isset($cmd)) {
+      if (!empty($cmd)) {
           // @TODO: Does _exec() inherit the $_SERVER environment? That must be why we have to pass to the docker compose calls...
-        if ($this->_exec($cmd)->wasSuccessful()) {
-          exit(0);
+        foreach ($cmd as $command) {
+          if (!$this->_exec($command)->wasSuccessful()) {
+            exit(1);
+          }
         }
-        else {
-          exit(1);
-        }
+        exit(0);
       }
     }
     elseif ($opts['mode'] == 'install.sh' || $opts['mode'] == 'manual') {
