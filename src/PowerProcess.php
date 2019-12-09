6d39abd2373dd36f1ea6ec2d8dad2ff2095f90cc
Jon Pugh <jon@thinkdrop.net>
2019-12-09 17:33:21 -0500

    Use Provision Style and pass environment to parent::run();

--- src/PowerProcess.php
+++ src/PowerProcess.php
@@ -6,7 +6,6 @@ use Psr\Log\LoggerAwareTrait;
 use Robo\Common\OutputAwareTrait;
 use Robo\Common\TimeKeeper;
 use Symfony\Component\Console\Output\Output;
-use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Process\Process as BaseProcess;
 
 class PowerProcess extends BaseProcess {
@@ -22,7 +21,7 @@ class PowerProcess extends BaseProcess {
     /**
      * @param $io Style
      */
-    public function setIo(SymfonyStyle $io) {
+    public function setIo(Style $io) {
         $this->io = $io;
     }
 
@@ -44,8 +43,18 @@ class PowerProcess extends BaseProcess {
 
 
     /**
-     * @param null $callback
-     * @return int
+     * Runs the process.
+     *
+     * @param callable|null $callback A PHP callback to run whenever there is some
+     *                                output available on STDOUT or STDERR
+     *
+     * @return int The exit status code
+     *
+     * @throws RuntimeException When process can't be launched
+     * @throws RuntimeException When process stopped after receiving signal
+     * @throws LogicException   In case a callback is provided and output has been disabled
+     *
+     * @final
      */
     public function run(callable $callback = null, array $env = []): int
     {
@@ -65,7 +74,8 @@ class PowerProcess extends BaseProcess {
                 $this->io->outputBlock(trim($line), false, false);
               }
             }
-        });
+        }, $env);
+
         $timer->stop();
         $this->duration = $timer->formatDuration($timer->elapsed());
 
