5a6c03a69a10ef8eac71ef0511153a4d8d447d7f
Jon Pugh <jon@thinkdrop.net>
2020-02-18 15:00:15 -0500

    Print a link button to each domain for the environment.

--- devmaster/modules/devshop/devshop_projects/task.tpl.php
+++ devmaster/modules/devshop/devshop_projects/task.tpl.php
@@ -1,12 +1,13 @@
 <div class="environment-tasks-alert alert-<?php print $task->status_class ?>" id="task-display-<?php print $task->nid; ?>">
     <div class="task-text">
-        <a href="<?php print $task->url; ?>" class="alert-link">
+          <?php foreach ($environment->domains as $domain): ?>
+            <a href="<?php print 'http://' . $domain; ?>" target="_blank" class="btn btn-link"><?php print 'http://' . $domain; ?></a>
+          <?php endforeach; ?>
             <i class="fa fa-<?php print $task->icon ?>"></i>
             <span class="type-name"><?php print $task->type_name ?></span>
             <span class="status-name small"><?php if ($task->task_status != HOSTING_TASK_QUEUED && $task->task_status != HOSTING_TASK_PROCESSING) print $task->status_name ?></span>
             &nbsp;
             <em class="time small"><i class="ago-icon fa fa-<?php if ($task->task_status == HOSTING_TASK_QUEUED || $task->task_status == HOSTING_TASK_PROCESSING) print 'clock-o'; else print 'calendar' ?>"></i> <time class="timeago" datetime="<?php print $task->task_timestamp ?>"><?php print $task->task_date ?></time></em>
-        </a>
     </div>
     <span class="progress">
         <div class="progress-bar progress-bar-striped progress-bar-info active" role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100">
