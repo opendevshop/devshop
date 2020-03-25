b67e20d85859095fe16256414f2cc7a819f8b90f
Jon Pugh <jon@thinkdrop.net>
2020-03-25 16:51:25 -0400

    fail prepareContainers if process fails.

--- RoboFile.php
+++ RoboFile.php
@@ -427,6 +427,10 @@ class RoboFile extends \Robo\Tasks {
     // @TODO: Figure out why PowerProcess::mustRun() fails so miserably: https://github.com/opendevshop/devshop/pull/541/checks?check_run_id=518074346#step:7:45
     $process->run();
 
+    if ($process->getExitCode() != 0) {
+      throw new \Exception('Process failed: ' . $process->getExitCodeText());
+    }
+
   }
 
   /**
