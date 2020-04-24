5625e38718e21d4c633fe7af8198df864708dd49
Jon Pugh <jon@thinkdrop.net>
2020-04-24 10:13:26 -0400

    Add a callGit() method to GitRepositoryAwareTrait.php to allow for easy command running.

--- src/DevShop/Component/ComposerCommon/src/GitRepositoryAwareTrait.php
+++ src/DevShop/Component/ComposerCommon/src/GitRepositoryAwareTrait.php
@@ -3,6 +3,7 @@
 namespace DevShop\Component\Common;
 
 use TQ\Git\Repository\Repository;
+use TQ\Vcs\Cli\CallResult;
 
 trait GitRepositoryAwareTrait
 {
@@ -37,5 +38,20 @@ trait GitRepositoryAwareTrait
             $this->setRepository();
         }
         return $this->repository;
+    }
+
+    /**
+     * Run a git command in the repository directory.
+     *
+     * @param   string  $method             The VCS command, e.g. show, commit or add
+     * @param   array   $arguments          The command arguments.
+     * @return  CallResult
+     * @example $this->callGit('status', array('--short'))
+     */
+    public function callGit($command, $arguments = array(), $message = "Git command failed.") {
+      /** @var $result CallResult */
+      $result = $this->getRepository()->getGit()->{$command}($this->getRepository()->getRepositoryPath(), $arguments);
+      $result->assertSuccess($message);
+      return $result;
     }
 }
