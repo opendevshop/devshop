5d1836946f57d6a0bedaea9787245e5f58a92068
Jon Pugh <jon@thinkdrop.net>
2020-05-12 19:25:07 -0400

    Add usable docker run command to the temporary build script so we can run what github actions runs.

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -15,3 +15,21 @@ docker build . --file=ansible/Dockerfile.ubuntu1804 --tag devshop/ansible:ubuntu
 # @TODO: Build new devshop/server dockerfiles.
 # docker build . --file=server/Dockerfile.centos7 --tag devshop/server:centos7
 # docker build . --file=server/Dockerfile.ubuntu1804 --tag devshop/server:ubuntu1804
+
+IMAGE=base
+OS=ubuntu1804
+DEVSHOP_SCRIPT_PATH=/root/install.sh
+DEVSHOP_SOURCE_PATH=/usr/share/devshop
+HOSTNAME=devshop.local.computer
+
+# Same as what's in github actions
+docker run \
+    --name devshop-${IMAGE}-${OS} \
+    --detach \
+    --privileged \
+    --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
+    --volume ${PWD}/install/install.sh:${DEVSHOP_SCRIPT_PATH} \
+    --volume ${PWD}:${DEVSHOP_SOURCE_PATH} \
+    --publish "80:80" \
+    --hostname ${HOSTNAME} \
+    devshop/${IMAGE}:${OS}
