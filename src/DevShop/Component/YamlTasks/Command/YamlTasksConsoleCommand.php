a1a6b24ba65bc560ba23db08133cb54182571c7d
Jon Pugh <jon@thinkdrop.net>
2020-07-27 11:52:42 -0400

    Add support for specifying working-directory in yaml tasks.

--- src/DevShop/Component/YamlTasks/Command/YamlTasksConsoleCommand.php
+++ src/DevShop/Component/YamlTasks/Command/YamlTasksConsoleCommand.php
@@ -427,9 +427,16 @@ class YamlTasksConsoleCommand extends BaseCommand
                 if ($task['show-output'] == false) {
                     $process->disableOutput();
                 }
-
-                // Set the dir relative to the task file.
-                $process->setWorkingDirectory(dirname($input->getOption('tasks-file')));
+                
+                // Allow "working-directory" property on a single task.
+                if ($task['working-directory']) {
+                    $this->io->text("Working directory <fg=white>{$task['working-directory']}</>");
+                    $process->setWorkingDirectory(dirname($input->getOption('tasks-file')) . "/" . $task['working-directory']);
+                }
+                else {
+                    // Set the dir relative to the task file.
+                    $process->setWorkingDirectory(dirname($input->getOption('tasks-file')));
+                }
 
                 // Set a commit status for this REF
                 $params = new \stdClass();
