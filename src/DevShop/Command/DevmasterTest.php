fea704b8ebfa4db1ce6b5707f8956cb5bc86ef73
Jon Pugh <jon@thinkdrop.net>
2020-08-03 15:38:45 -0400

    Default to devshop-control-1.x folder

--- src/DevShop/Command/DevmasterTest.php
+++ src/DevShop/Command/DevmasterTest.php
@@ -25,7 +25,7 @@ class DevmasterTest extends Command {
         '',
         InputOption::VALUE_OPTIONAL,
         'The root path to the site to test, if @hostmaster alias is not found.',
-        '/var/aegir/devmaster-1.x'
+        '/var/aegir/devshop-control-1.x'
       )
       ->addOption(
         'name',
