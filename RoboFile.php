df3eed872f5f0d3ccb73998937200543e6619242
Jon Pugh <jon@thinkdrop.net>
2020-08-04 12:10:45 -0400

    Use --prefer-source when preparing sourcecode.

--- RoboFile.php
+++ RoboFile.php
@@ -262,7 +262,7 @@ class RoboFile extends \Robo\Tasks {
     // Run composer install on devmaster stack so it's ready before the container launches and devmaster install command is faster.
     $this->taskExecStack()
       ->dir('src/DevShop/Templates/DevShopControlTemplate')
-      ->exec("composer install --ansi")
+      ->exec("composer install --prefer-source --ansi")
       ->run();
 
     // Set git remote urls
