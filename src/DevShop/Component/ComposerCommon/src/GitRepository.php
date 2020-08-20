7cdcfae2f366db3aa0618ac56d2ee262045947b2
Jon Pugh <jon@thinkdrop.net>
2020-08-20 12:13:33 -0400

    Add a "isDetached()" method to DevShop\Component\Common\Repository.

--- src/DevShop/Component/ComposerCommon/src/GitRepository.php
+++ src/DevShop/Component/ComposerCommon/src/GitRepository.php
@@ -3,6 +3,7 @@
 namespace DevShop\Component\Common;
 
 use TQ\Git\Repository\Repository;
+use TQ\Vcs\Cli\CallResult;
 
 class GitRepository extends Repository {
 
@@ -19,5 +20,22 @@ class GitRepository extends Repository {
     $result = $this->getRepository()->getGit()->{$command}($this->getRepository()->getRepositoryPath(), $arguments);
     $result->assertSuccess($message);
     return $result;
+  }
+
+  /**
+   * Returns TRUE if the working directory is in 'detached HEAD' state.
+   * https://stackoverflow.com/questions/52221558/programmatically-check-if-head-is-detached
+   *
+   * @return  boolean
+   */
+  public function isDetached() {
+    /** @var $result CallResult */
+    $result = $this->getGit()->{'symbolic-ref'}($this->getRepositoryPath(), array(
+      '-q',
+      'HEAD'
+    ));
+
+    // If command failed, HEAD is not on a branch, aka detached.
+    return 0 !== $result->getReturnCode();
   }
 }
