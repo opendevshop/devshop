d5260f28b9dd2bc5852e60bdf5baf2c999e806c3
Jon Pugh <jon@thinkdrop.net>
2020-08-27 10:21:20 -0400

    Finish creating options for deploy command from stages. Create stages and Default stages list.

--- src/DevShop/Component/Deploy/DeployCommand.php
+++ src/DevShop/Component/Deploy/DeployCommand.php
@@ -14,10 +14,11 @@ namespace DevShop\Component\Deploy;
 use Composer\Composer;
 use Composer\Config;
 use DevShop\Component\Common\ComposerRepositoryAwareTrait;
+use DevShop\Component\Deploy\DeployStages;
 use Robo\Common\OutputAwareTrait;
 use Symfony\Component\Console\Command\Command as BaseCommand;
-use DevShop\Component\Deploy\DeployStages;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 
@@ -29,12 +30,8 @@ use Symfony\Component\Console\Style\SymfonyStyle;
  */
 class DeployCommand extends BaseCommand
 {
-    use ComposerRepositoryAwareTrait;
 
-    /**
-     * @var array default DeployStages
-     */
-    private $defaultStages = [];
+    use ComposerRepositoryAwareTrait;
 
     /**
      * @var SymfonyStyle
@@ -42,71 +39,93 @@ class DeployCommand extends BaseCommand
     private $io;
 
 
-  /**
-   * {@inheritdoc}
-   */
-  protected function configure()
-  {
-    $this
-      ->setName('deploy')
-      ->setDescription('Run the deploy stages for this application, contingent on environment configuration and .')
-      ->setHelp(<<<'EOF'
+    /**
+     * {@inheritdoc}
+     */
+    protected function configure()
+    {
+        $this
+          ->setName('deploy')
+          ->setDescription(
+            'Run the deploy stages for this application, contingent on environment configuration and .'
+          )
+          ->setHelp(
+            <<<'EOF'
 The <info>%command.name%</info> runs the commands defined in a project's composer.json:extras.deploy.stages
 EOF
-      )
-    ;
-    // @TODO Add Build stage options
-  }
+          );
 
-  /**
-   * {@inheritdoc}
-   */
-  public function initialize(InputInterface $input, OutputInterface $output) {
-    // Prepare object.
-    $this->io = new SymfonyStyle($input, $output);
-    $this->setComposerConfig();
-    $this->defaultStages = DeployStages::defaultStages();
-
-    // Load the stages from the current composer project.
-    if ($this->io->isVerbose()){
-      $this->io->section('Initializing Deploy Command');
-      $verbose_rows[] = ['PWD', getenv('PWD')];
-      $verbose_rows[] = ['Repository Root', $this->getRepository()->getRepositoryPath()];
-      $verbose_rows[] = ['Composer Project', $this->getComposerConfig()->name()];
-
-      $this->io->table(['Debug Information'], $verbose_rows);
+        // Add CLI options for default stages.
+        foreach ([false, true] as $skip) {
+            $i = 0;
+            foreach (DeployStages::getStages() as $stage_name => $description) {
+                $i++;
+                $this
+                  ->addOption(
+                    $skip
+                      ? "skip-$stage_name"
+                      : $stage_name
+                    ,
+                    null,
+                    InputOption::VALUE_NONE,
+                    $skip
+                      ? "Skip Stage $i: $stage_name: $description"
+                      : "Stage $i: $stage_name: $description"
+                  );
+            }
+        }
     }
 
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public function initialize(InputInterface $input, OutputInterface $output)
+    {
+        // Prepare object.
+        $this->io = new SymfonyStyle($input, $output);
+        $this->setComposerConfig();
 
-  /**
-   * Load deploy stage config from composer.json
-   */
-  public function loadStages() {
-      if (empty($this->getComposerConfig()->extra()->deploy)) {
-        // @TODO: Allow Fail or Warn
-        $this->io->warning("No deploy stages found in composer.json:extra.deploy");
-      }
-  }
+        // Load the stages from the current composer project.
+        if ($this->io->isVerbose()) {
+            $this->io->section('Initializing Deploy Command');
+            $verbose_rows[] = ['PWD', getenv('PWD')];
+            $verbose_rows[] = [
+              'Repository Root',
+              $this->getRepository()->getRepositoryPath(),
+            ];
+            $verbose_rows[] = [
+              'Composer Project',
+              $this->getComposerConfig()->name(),
+            ];
 
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function execute(InputInterface $input, OutputInterface $output)
-  {
-    $this->io->title('Deploy');
-
-    // Validate Stages
-    $this->loadStages();
-
-
-    foreach ($this->defaultStages as $name => $description) {
-      $rows[] = [
-        $name, $description
-      ];
+            $this->io->table(['Debug Information'], $verbose_rows);
+        }
     }
-    $this->io->table(['Stage', 'Command'], $rows);
-    return 0;
-  }
+
+    /**
+     * Load deploy stage config from composer.json
+     */
+    public function loadStages()
+    {
+        if (empty($this->getComposerConfig()->extra()->deploy)) {
+            // @TODO: Allow Fail or Warn
+            $this->io->warning(
+              "No deploy stages found in composer.json:extra.deploy"
+            );
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $this->io->title('Deploy');
+
+        // Validate Stages
+        $this->loadStages();
+
+        return 0;
+    }
+
 }
--- src/DevShop/Component/Deploy/DeployStages.php
+++ src/DevShop/Component/Deploy/DeployStages.php
@@ -4,7 +4,7 @@ namespace DevShop\Component\Deploy;
 
 class DeployStages {
 
-    const DEFAULT_STAGES = [
+    const STAGES = [
         'git' => 'Get the source code.',
         'build' => 'Prepare the source code and services.',
         'install' => 'Install the application data.',
@@ -12,12 +12,34 @@ class DeployStages {
         'test' => 'Run a command to verify functionality of the site.'
     ];
 
+    const DEFAULT_DEPLOY = [
+        'git',
+        'build',
+        'deploy',
+    ];
+
     /**
-     * Get the list of default Stages.
+     * Get the list of all Stages.
      * @return string[] List of stages with key being stage name, value the description.
      */
-    public static function defaultStages (){
-      return self::DEFAULT_STAGES;
+    public static function getStages(){
+      return self::STAGES;
     }
 
+    /**
+     * Get the list of stages run by default.
+     * @return string[] List of stages that will be run by default during a deploy.
+     */
+    public static function getDefaultDeploy(){
+      return self::DEFAULT_DEPLOY;
+    }
+
+    /**
+     * Get the list of stages that are run by default.
+     *
+     * @return string[] List of stages that run by default during a "deploy" command.
+     */
+    public static function isDefaultStage($stage_name){
+      return in_array($stage_name, self::getDefaultDeploy());
+    }
 }
