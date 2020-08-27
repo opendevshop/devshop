a1f4832b400e801faa0815f266fa79f63c2f9c24
Jon Pugh <jon@thinkdrop.net>
2020-08-27 13:12:05 -0400

    Add missing fil DeployStage.

---
+++ src/DevShop/Component/Deploy/DeployStage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace DevShop\Component\Deploy;
+
+class DeployStage {
+
+    /**
+     * @var string The stage name.
+     */
+    public $name = 'deploy';
+
+    /**
+     * @var string The stage command
+     */
+    private $command = 'echo "DeployStage command not set. Define DeployStage::command property."';
+
+    public function __construct($name, $command = null)
+    {
+        $this->name = $name;
+        $this->command = $command?: $this->command;
+    }
+
+    /**
+     * Run the command for this stage.
+     * @TODO: Use Process component or ProcessAwareTrait.
+     */
+    public function runStage() {
+        print shell_exec($this->command);
+    }
+
+    /**
+     * @return string
+     */
+    public function command(){
+        return $this->command;
+    }
+}
