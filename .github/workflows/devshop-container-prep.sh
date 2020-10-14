6d9a98d999a22c1b17cf5db5c7b398ef39c8f348
Jon Pugh <jon@thinkdrop.net>
2020-10-14 15:55:19 -0400

    Set -x in the container prep script so we can see what is being run.

--- .github/workflows/devshop-container-prep.sh
+++ .github/workflows/devshop-container-prep.sh
@@ -7,8 +7,9 @@ GIT_REF=${GITHUB_HEAD_REF:-"1.x"}
 devshop-logo "Preparing DevShop Control for CI Tests"
 
 devshop-log "Adding repos to composer global config."
+set -x
 composer config --global repo.devshop_devmaster {"path","/usr/share/devshop/devmaster"}
 cd "$DEVSHOP_PATH/src/DevShop/Component/DevShopControlTemplate"
 git init
 git checkout -b $GIT_REF
-composer require "devshop/devmaster:$GIT_REF-dev"
+composer require "devshop/devmaster:$GIT_REF-dev" --no-progress --no-suggest
