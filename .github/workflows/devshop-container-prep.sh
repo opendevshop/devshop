df73ff4bbeeb81e62228c3038f77d042ab456e79
Jon Pugh <jon@thinkdrop.net>
2020-10-14 14:22:52 -0400

    Create a container prep script to setup composer repos in the container.

---
+++ .github/workflows/devshop-container-prep.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+DEVSHOP_PATH="$( cd "$(dirname "$0")"/../../bin ; pwd -P )"
+PATH="$DEVSHOP_PATH:$PATH"
+
+devshop-logo "Preparing DevShop Control for CI Tests"
+
+devshop-log "Adding repos to composer global config."
+composer config --global repo.devshop_devmaster {"path","/usr/share/devshop/devmaster"}
+cd "$DEVSHOP_PATH/src/DevShop/Components/DevShopControl/Template"
+git init
+git checkout -b "${GITHUB_HEAD_REF:-1.x}"
+composer require "devshop/devmaster:${GITHUB_HEAD_REF:-1.x}-dev"
