c3abc9cb6cf1fa99d4af6a3e3c377f58fcf340c0
Jon Pugh <jon@thinkdrop.net>
2020-10-14 14:25:41 -0400

    Wrong path.

--- .github/workflows/devshop-container-prep.sh
+++ .github/workflows/devshop-container-prep.sh
@@ -7,7 +7,7 @@ devshop-logo "Preparing DevShop Control for CI Tests"
 
 devshop-log "Adding repos to composer global config."
 composer config --global repo.devshop_devmaster {"path","/usr/share/devshop/devmaster"}
-cd "$DEVSHOP_PATH/src/DevShop/Components/DevShopControl/Template"
+cd "$DEVSHOP_PATH/src/DevShop/Components/DevShopControlTemplate"
 git init
 git checkout -b "${GITHUB_HEAD_REF:-1.x}"
 composer require "devshop/devmaster:${GITHUB_HEAD_REF:-1.x}-dev"
