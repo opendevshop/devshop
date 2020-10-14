3653082567a314d301ff4fe9913f6a34f2d0d369
Jon Pugh <jon@thinkdrop.net>
2020-10-14 15:47:37 -0400

    Fix passing of GITHUB_HEAD_REF to the container and to the script.

--- .github/workflows/devshop-container-prep.sh
+++ .github/workflows/devshop-container-prep.sh
@@ -2,6 +2,7 @@
 set -e
 DEVSHOP_PATH="$( cd "$(dirname "$0")"/../.. ; pwd -P )"
 PATH="$DEVSHOP_PATH/bin:$PATH"
+GIT_REF=${GITHUB_HEAD_REF:-"1.x"}
 
 devshop-logo "Preparing DevShop Control for CI Tests"
 
@@ -9,5 +10,5 @@ devshop-log "Adding repos to composer global config."
 composer config --global repo.devshop_devmaster {"path","/usr/share/devshop/devmaster"}
 cd "$DEVSHOP_PATH/src/DevShop/Component/DevShopControlTemplate"
 git init
-git checkout -b "${GITHUB_HEAD_REF:-1.x}"
-composer require "devshop/devmaster:${GITHUB_HEAD_REF:-1.x}-dev"
+git checkout -b $GIT_REF
+composer require "devshop/devmaster:$GIT_REF-dev"
