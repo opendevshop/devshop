7367831dd2798e3addb0ca656a39ae6853cc7c89
Jon Pugh <jon@thinkdrop.net>
2020-10-22 12:38:48 -0400

    Cherry pick changes from feature/ch139/ansible-cloud: - Show 3 git log items. - Move devshop control pseudo git repo prep to shell script. - Clean up devshop.yml env vars. - Set supervisor to true by default, false in CI. - create a vars.ci.yml file to make management easier.

--- .github/workflows/devshop-container-prep.sh
+++ .github/workflows/devshop-container-prep.sh
@@ -12,4 +12,10 @@ composer config --global repo.devshop_devmaster {"path","/usr/share/devshop/devm
 cd "$DEVSHOP_PATH/src/DevShop/Component/DevShopControlTemplate"
 git init
 git checkout -b $GIT_REF
+git remote add origin https://github.com/devshop-packages/devshop-control-template
+git add .gitignore
+git add -A
+git config --global user.email "github@opendevshop.com"
+git config --global user.name "GitHub Actions"
+git commit -m 'Temporary commit'
 composer require "devshop/devmaster:$GIT_REF-dev" --no-progress --no-suggest
