766562fa3c2918143111f7384a4c92144f78537d
Jon Pugh <jon@thinkdrop.net>
2020-11-16 18:32:44 -0500

    Set --entrypoint on the install script docker run to be sure it's using the full path with the current code.

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -3,4 +3,4 @@
 # bash .github/scripts/install-container.sh
 set -e
 docker-compose --file docker/docker-compose.yml build base
-docker run --name install-server-test --hostname devshop.local.computer --env DEVSHOP_DOCKER_COMMAND_RUN="bash /usr/share/devshop/install.sh" --publish 80:80 --privileged --volume ./:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base bash /usr/share/devshop/install/install.sh
+docker run --name install-server-test --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --env DEVSHOP_DOCKER_COMMAND_RUN="bash /usr/share/devshop/install.sh" --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base bash /usr/share/devshop/install/install.sh
