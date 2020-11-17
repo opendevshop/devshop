952752d0556f1d72e11cfe51c5128840305d50d7
Jon Pugh <jon@thinkdrop.net>
2020-11-17 11:14:58 -0500

    Clean up the install-sh-test.sh script to be more readable and only include bare minimum options. This container is for testing the "standalone install script", so it should NOT have any additional scripts or files available.

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -1,8 +1,27 @@
 # Build a core container and run the install script in it.
 # Run from repository root:
 # bash .github/scripts/install-container.sh
-set -e
-docker-compose --file docker/docker-compose.yml build base
-docker run --name install-server-test --detach --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base
+echo "Installing $DEVSHOP_VERSION..."
+set -ex
 
-docker exec --env="ANSIBLE_PLAYBOOK_COMMAND_OPTIONS=--inventory /usr/share/devshop/roles/devshop.server/inventory" install-server-test bash /usr/share/devshop/install/install.sh
+# Rebuild a base container to include this PR's systemd scripts.
+docker-compose --file docker/docker-compose.yml build base
+
+# Remove existing install server test containers.
+docker kill install-server-test || true
+
+# Launch a devshop/base container with this PR's install.sh script inside.
+docker run \
+  --name install-server-test \
+  --detach --privileged --rm \
+  --hostname devshop.local.computer \
+  --publish 80:80 \
+  --volume $PWD/install/install.sh:/tmp/devshop-install.sh \
+  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
+  devshop/base
+
+docker exec \
+  --env="ANSIBLE_PLAYBOOK_COMMAND_OPTIONS=--inventory devshop.server" \
+  --env="DEVSHOP_VERSION" \
+  install-server-test \
+  bash /tmp/devshop-install.sh \
