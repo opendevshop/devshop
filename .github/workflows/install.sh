bb80462feea73739d42660686422a878a3971e80
Jon Pugh <jon@thinkdrop.net>
2020-11-16 21:24:10 -0500

    Use two steps: launch container, then run install.sh/

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -3,4 +3,6 @@
 # bash .github/scripts/install-container.sh
 set -e
 docker-compose --file docker/docker-compose.yml build base
-docker run --name install-server-test --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base bash /usr/share/devshop/install/install.sh
+docker run --name install-server-test --detach --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base
+
+docker exec -ti install-server-test bash /usr/share/devshop/install/install.sh
