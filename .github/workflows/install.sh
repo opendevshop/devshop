a9ac21a7cf1c570584d46cf16920d58ebc620e1a
Jon Pugh <jon@thinkdrop.net>
2020-11-16 21:32:01 -0500

    No -ti

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -5,4 +5,4 @@ set -e
 docker-compose --file docker/docker-compose.yml build base
 docker run --name install-server-test --detach --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base
 
-docker exec -ti install-server-test bash /usr/share/devshop/install/install.sh
+docker exec install-server-test bash /usr/share/devshop/install/install.sh
