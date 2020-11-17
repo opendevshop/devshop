aebe671861d1b1112894219ef41a87dd157e8d98
Jon Pugh <jon@thinkdrop.net>
2020-11-16 21:36:22 -0500

    add --inventory ANSIBLE option.

--- .github/workflows/install.sh
+++ .github/workflows/install.sh
@@ -5,4 +5,4 @@ set -e
 docker-compose --file docker/docker-compose.yml build base
 docker run --name install-server-test --detach --hostname devshop.local.computer --entrypoint /usr/share/devshop/docker/bin/docker-systemd-entrypoint --publish 80:80 --privileged --volume $PWD:/usr/share/devshop --volume /sys/fs/cgroup:/sys/fs/cgroup:ro --rm devshop/base
 
-docker exec install-server-test bash /usr/share/devshop/install/install.sh
+docker exec --env="ANSIBLE_PLAYBOOK_COMMAND_OPTIONS=--inventory /usr/share/devshop/roles/devshop.server/inventory" install-server-test bash /usr/share/devshop/install/install.sh
