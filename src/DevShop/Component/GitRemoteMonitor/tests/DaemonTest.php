3b1717dae1d351a85081c453f98ab47ca2305168
Jon Pugh <jon@thinkdrop.net>
2021-01-14 11:34:28 -0500

    Adding phpunit tests!

---
+++ src/DevShop/Component/GitRemoteMonitor/tests/DaemonTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace DevShop\Component\GitRemoteMonitor;
+
+use PHPUnit\Framework\TestCase;
+
+final class RemoteMonitorDaemonTest extends TestCase
+{
+  public function testGetInstance(): void
+  {
+    RemoteMonitorDaemon::setFilename(__FILE__);
+    $this->assertInstanceOf(
+      \Core_Daemon::class,
+      RemoteMonitorDaemon::getInstance(),
+      'Daemon instance failed: no filename.'
+    );
+  }
+}
+
+
+
