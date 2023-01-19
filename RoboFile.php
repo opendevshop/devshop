3b6ea378c9a31003bc7e98b328e97bf175884943
Jon Pugh <jon@thinkdrop.net>
2023-01-19 16:02:10 -0500

    Go back to ubuntu. With GG, GITHUB ACTIONS fails on mysql!   msg: Service is in unknown state

--- RoboFile.php
+++ RoboFile.php
@@ -240,7 +240,7 @@ class RoboFile extends \Robo\Tasks {
   public function build($folder = 'docker', $service = 'all', $opts = [
       'docker-image' => 'devshop/server:latest',
       'scratch' => FALSE,
-      'from' => 'geerlingguy/docker-ubuntu1804-ansible',
+      'from' => 'ubuntu:18.04',
       'build-command' => NULL,
       'os' => NULL,
       'vars' => '',
