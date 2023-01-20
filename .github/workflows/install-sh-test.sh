797ad44ba1f5a68ebd921ab6f42a4923e68d31f1
Jon Pugh <jon@thinkdrop.net>
2023-01-19 19:08:14 -0500

    Docker Build Again (#704)
    * Remove composer update hook to also run composer update in control site. 
    
    * put back bin-tools so they are available in bin/ and fix path lookup, aligning with composer's new bin trick.
    
    * Put back Ansible playbook run in docker build command so we can save images.
    
    * Fix up docker-systemd-prepare file.
    
    * Adding run-quiet script to main scripts.
    
    * Run both devshop-install-prerequisites (to install ansible) AND docker-systemd-prepare to ensure fully functioning container.
    
    * Default to ubuntu1804 when using docker/docker-compose.yml
    
    * fix entry points, missing env vars in docker compose, build args, etc.
    
    * Fix robo up and build.
    
    * output more debugging info in GitHub actions.
    
    * Get systems working right in GitHub actions and locally, by REMOVING the systems volume! ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯
    
    * thanks https://github.com/geerlingguy/docker-ubuntu2004-ansible/issues/18#issuecomment-1104942607

--- .github/workflows/install-sh-test.sh
+++ .github/workflows/install-sh-test.sh
@@ -33,7 +33,6 @@ docker run \
   --hostname $DEVSHOP_SERVER_HOSTNAME \
   --publish 80:80 \
   --volume $PWD/build/install.sh:/tmp/devshop-install.sh \
-  --volume /sys/fs/cgroup:/sys/fs/cgroup:ro \
   ubuntu/container
 
 docker exec \
--- RoboFile.php
+++ RoboFile.php
@@ -59,7 +59,7 @@ class RoboFile extends \Robo\Tasks {
     'run-command' => 'DEVSHOP_DOCKER_COMMAND_RUN',
 
     // Used in docker compose image.
-    'from' => 'FROM_IMAGE',
+    'from' => 'DEVSHOP_CONTAINER_FROM',
     'os' => 'OS_VERSION',
     'dockerfile' => 'DOCKERFILE',
     'compose-file' => 'COMPOSE_FILE',
@@ -226,7 +226,8 @@ class RoboFile extends \Robo\Tasks {
    * @param folder The folder to run 'docker-compose build' in. Use "all" to build in folder 'docker', then 'roles'.
    * @param service The service to build. passed to 'docker-compose build $SERVICE'. Use "all" to build all services.
    *
-   * @option $tag The string to tag the resulting container with.
+   * @option $docker-image The label to use when building or running the container.
+   * @option $scratch Set --scratch to set docker-image=ubuntu2004
    * @option $from The image to use to build the docker image FROM. Ignored if "os" is set.
    * @option $os An OS "slug" for any of the geerlingguy/docker-*-ansible images: https://hub.docker.com/u/geerlingguy/
    * @option $vars Ansible vars to pass to --extra-vars option.
@@ -238,7 +239,8 @@ class RoboFile extends \Robo\Tasks {
    */
   public function build($folder = 'docker', $service = 'all', $opts = [
       'docker-image' => 'devshop/server:latest',
-      'from' => NULL,
+      'scratch' => FALSE,
+      'from' => 'ubuntu:18.04',
       'build-command' => NULL,
       'os' => NULL,
       'vars' => '',
@@ -248,6 +250,16 @@ class RoboFile extends \Robo\Tasks {
       'environment' => [],
       'install-at-runtime' => FALSE,
   ]) {
+
+    try {
+      $branch = $this->getRepository()->getCurrentBranch();
+      $remote = $this->getRepository()->getCurrentRemoteName();
+      $remote_url = $this->getRepository()->getCurrentRemoteUrl();
+      $this->say("Current code branch <comment>{$branch}</comment> remote {$remote_url}");
+    } catch (\Exception $e) {
+      $this->io()->error("No upstream configured for branch '$branch'. Please set one with the command 'git branch --track $branch' or 'git push -u origin $branch'");
+      exit(1);
+    }
 
     if ($service == "all") {
       $service = '';
@@ -260,7 +272,11 @@ class RoboFile extends \Robo\Tasks {
 
     // Define docker-image (name for the "image" in docker-compose)
     // Set FROM_IMAGE and DEVSHOP_DOCKER_IMAGE if --os option is used. (and --from was not used)
-    if (empty($opts['from']) && $opts['os'] !== NULL) {
+    if ($opts['scratch']) {
+      $opts['from'] = "ubuntu:18.04";
+    }
+
+    if ($opts['os'] !== NULL) {
       $opts['from'] = "geerlingguy/docker-{$opts['os']}-ansible";
       $opts['docker-image'] = 'devshop/server:' . $opts['os'];
     }
@@ -288,6 +304,11 @@ class RoboFile extends \Robo\Tasks {
 
     // Runtime Environment for the docker-compose build command.
     $env_build = $this->generateEnvironmentArgs($opts);
+
+    $env_build['ANSIBLE_EXTRA_VARS'] = "devshop_version={$branch} devshop_cli_version={$branch}";
+    $env_build['ANSIBLE_PLAYBOOK_COMMAND_OPTIONS_ARG'] = '--extra-vars=@/usr/share/devshop/vars.development.yml';
+
+    print_r($env_build);
 
     $provision_io = new \DevShop\Component\PowerProcess\PowerProcessStyle($this->input(), $this->output());
     $process = new \DevShop\Component\PowerProcess\PowerProcess("docker-compose build --no-cache $service", $provision_io);
@@ -387,7 +408,7 @@ class RoboFile extends \Robo\Tasks {
     'docker-image' => 'devshop/server:latest',
     // The OS "slug" to use instead of devshop/server:ubuntu1804. If specified, "docker-image" option will be ignored.
     'os' => NULL,
-    'from' => NULL,
+    'from' => 'ubuntu:18.04',
     'vars' => '',
     'tags' => '',
     'skip-tags' => '',
@@ -472,7 +493,7 @@ class RoboFile extends \Robo\Tasks {
       $opts['from'] = "geerlingguy/docker-{$opts['os']}-ansible";
       $opts['docker-image'] = 'devshop/server:' . $opts['os'];
     }
-    else {
+    elseif (empty($opts['from'])) {
       $opts['from'] = $opts['docker-image'];
     }
 
@@ -534,7 +555,7 @@ class RoboFile extends \Robo\Tasks {
       $test_command = '';
       if ($opts['test']) {
         // Do not run a playbook on docker-compose up, because it will launch as a separate process and we won't know when it ends.
-        $cmd[] = "docker-compose run container {$docker_command}";
+        $cmd[] = "docker-compose run devshop.server {$docker_command}";
         $env_run['DEVSHOP_DOCKER_COMMAND_RUN'] = $docker_command;
 
         $test_command = "su aegir --command /usr/share/devshop/tests/devshop-tests.sh";
@@ -546,7 +567,7 @@ class RoboFile extends \Robo\Tasks {
         $test_command = "/usr/share/devshop/tests/devshop-tests-upgrade.sh";
       }
       else {
-        $cmd[] = "docker-compose up --detach --force-recreate container";
+        $cmd[] = "docker compose up --detach --force-recreate devshop.server";
         if (!$opts['no-follow']) {
           $cmd[] = "docker-compose logs -f";
         }
@@ -557,6 +578,9 @@ class RoboFile extends \Robo\Tasks {
 
       // Runtime Environment for the $cmd list.
       $env_run = $this->generateEnvironmentArgs($opts);
+      $env_run['ANSIBLE_PLAYBOOK_COMMAND_OPTIONS'] = '--extra-vars=@/usr/share/devshop/vars.development.yml';
+      print_r($env_run);
+
       $extra_vars = array();
 
       // Set devshop_version and cli_repo here because every local dev environment is different.
@@ -650,13 +674,15 @@ class RoboFile extends \Robo\Tasks {
   /**
    * Run a command in the devshop container.
    */
-  public function exec($cmd = '', $opts = ['user' => 'aegir']) {
-    return $this->_exec("docker-compose exec -T \
+  public function exec($cmd = '', $opts = ['user' => 'root']) {
+    return $this->taskExec("docker-compose exec -T \
       --env ANSIBLE_TAGS=runtime \
       --env ANSIBLE_SKIP_TAGS \
       --env ANSIBLE_VARS \
       --user {$opts['user']} \
-      devshop $cmd")->getExitCode();
+      devshop.server $cmd")
+        ->dir('docker')
+        ->run();
   }
 
   /**
@@ -681,13 +707,13 @@ class RoboFile extends \Robo\Tasks {
       // Remove devmaster site folder
       $version = self::DEVSHOP_LOCAL_VERSION;
       $uri = self::DEVSHOP_LOCAL_URI;
-      $this->_exec("cd docker && docker-compose exec container rm -rf /usr/share/devshop/src/DevShop/Control/web/sites/{$uri}");
+      $this->_exec("cd docker && docker-compose exec devshop.server rm -rf /usr/share/devshop/src/DevShop/Control/web/sites/{$uri}");
       $this->_exec('cd docker && docker-compose kill');
       $this->_exec('cd docker && docker-compose rm -fv');
     }
 
     // Don't run when -n is specified,
-    if (!$this->input()->isInteractive() || $this->confirm("Destroy container home directory? (aegir-home)")) {
+    if (!$this->input()->isInteractive() || $this->confirm("Destroy devshop.server home directory? (aegir-home)")) {
       if ($this->_exec("rm -rf aegir-home")->wasSuccessful()) {
         $this->say("Entire aegir-home folder deleted.");
       }
@@ -732,7 +758,7 @@ class RoboFile extends \Robo\Tasks {
    */
   public function watchdog() {
     $user = 'aegir';
-    $this->taskExec("docker-compose exec --user $user -T container drush @hostmaster wd-show --tail --extended")
+    $this->taskExec("docker-compose exec --user $user -T devshop.server drush @hostmaster wd-show --tail --extended")
         ->dir("docker")
         ->run();
   }
@@ -754,10 +780,10 @@ class RoboFile extends \Robo\Tasks {
   public function shell($user = 'aegir') {
 
     if ($user) {
-        $process = new \Symfony\Component\Process\Process("docker-compose exec --user $user container bash");
+        $process = new \Symfony\Component\Process\Process("docker-compose exec --user $user devshop.server bash");
     }
     else {
-        $process = new \Symfony\Component\Process\Process("docker-compose exec container bash");
+        $process = new \Symfony\Component\Process\Process("docker-compose exec devshop.server bash");
     }
     $process->setTty(TRUE);
     $process->setTimeout(NULL);
@@ -779,16 +805,16 @@ class RoboFile extends \Robo\Tasks {
     // If running in CI, create the test-artifacts directory and ensure ownership first.
     // @TODO: Move logic to a special CI container.
     if (!empty($_SERVER['CI'])) {
-      $commands[] = "docker-compose exec $no_tty container mkdir -p /var/aegir/test-artifacts";
-      $commands[] = "docker-compose exec $no_tty container chown aegir:aegir /var/aegir/test-artifacts -R";
-      $commands[] = "docker-compose exec $no_tty container chmod 777 /var/aegir/test-artifacts -R";
+      $commands[] = "docker-compose exec $no_tty devshop.server mkdir -p /var/aegir/test-artifacts";
+      $commands[] = "docker-compose exec $no_tty devshop.server chown aegir:aegir /var/aegir/test-artifacts -R";
+      $commands[] = "docker-compose exec $no_tty devshop.server chmod 777 /var/aegir/test-artifacts -R";
     }
 
     if ($opts['reinstall']) {
-      $commands[] = "docker-compose exec $no_tty --user $user container drush @hostmaster provision-install --force-reinstall";
+      $commands[] = "docker-compose exec $no_tty --user $user devshop.server drush @hostmaster provision-install --force-reinstall";
     }
 
-    $commands[] = "docker-compose exec $no_tty --user $user container /usr/share/devshop/tests/devshop-tests.sh";
+    $commands[] = "docker-compose exec $no_tty --user $user devshop.server /usr/share/devshop/tests/devshop-tests.sh";
     $provision_io = new \DevShop\Component\PowerProcess\PowerProcessStyle($this->input, $this->output);
     foreach ($commands as $command) {
       $process = new \DevShop\Component\PowerProcess\PowerProcess($command, $provision_io);
@@ -815,7 +841,7 @@ class RoboFile extends \Robo\Tasks {
    * Get a one-time login link to Devamster.
    */
   public function login($user = 'aegir') {
-    $this->taskExec("docker-compose exec --user $user -T container /usr/share/devshop/bin/drush @hostmaster uli")
+    $this->taskExec("docker-compose exec --user $user -T devshop.server /usr/share/devshop/bin/drush @hostmaster uli")
         ->dir("docker")
         ->run();
     ;
