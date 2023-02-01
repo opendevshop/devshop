7127e815caec1a6af913c241eb32b235d622748a
Jon Pugh <jon@thinkdrop.net>
2023-01-31 21:48:30 -0500

    Remove comments.

--- src/DevShop/Command/DevmasterTest.php
+++ src/DevShop/Command/DevmasterTest.php
@@ -85,45 +85,6 @@ class DevmasterTest extends Command {
     $uri = $input->getOption('uri');
     $root = $input->getOption('root');
 
-//    // @TODO: This is all to get these tests running on Drupal6 devmaster! should be able to remove this for drupal7
-//    // Lookup password from @hostmaster alias
-//    $output->writeln('Looking up hostmaster database credentials...');
-//
-//    $process = new Process('drush @hostmaster sql-conf --format=var_export --show-passwords');
-//    $process->mustRun();
-//    $db_var_export = $process->getOutput();
-//    $db = (object) eval("return {$db_var_export};");
-//
-//    // Write to local.settings.php
-//    $path = "{$root}/sites/{$uri}/local.settings.php";
-//    $settings_default_path = "{$root}/sites/default/settings.php";
-//    $output->writeln("Writing db credentials to $path...");
-//
-//    $db_url = "{$db->driver}://{$db->username}:{$db->password}@{$db->host}:{$db->port}/{$db->database}";
-//
-//    $output = <<<PHP
-//<?php
-//  \$databases['default']['default'] = $db_var_export;
-//  \$db_url = "$db_url";
-//
-//PHP;
-//    $fs = new Filesystem();
-//    $fs->dumpFile($path, trim($output));
-//    $fs->dumpFile($settings_default_path, trim($output));
-//
-//    // Run composer install
-////    $process = new Process('composer install --no-progress --no-suggest --ansi');
-////    $process->setTimeout(NULL);
-////    $process->setWorkingDirectory($input->getOption('behat-path'));
-//
-//    $process->run(function ($type, $buffer) {
-//      if (Process::ERR === $type) {
-//        echo $buffer;
-//      } else {
-//        echo $buffer;
-//      }
-//    });
-
     // Show git info
     $process = new Process('git log -3');
     $process->setWorkingDirectory($input->getOption('behat-path'));
