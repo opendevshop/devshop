ceb064f9de48381785f53299d647e8cdf2aa515c
Jon Pugh <jon@thinkdrop.net>
2023-04-20 15:16:58 -0400

    Switch to use git-branch task

--- devmaster/modules/devshop/devshop_projects/templates/environment.tpl.php
+++ devmaster/modules/devshop/devshop_projects/templates/environment.tpl.php
@@ -400,10 +400,10 @@
                                 <span class="caret"></span>
                             </button>
                             <ul class="dropdown-menu btn-git-ref" role="menu">
-                              <?php if (user_access('create platform git-checkout task') || user_access('create platform git-tag task')): ?>
+                              <?php if (user_access('create platform git-branch task') || user_access('create platform git-tag task')): ?>
                                 <li class="create-git-ref"><label><?php print t('Create'); ?>
-                              <?php if (user_access('create platform git-checkout task')): ?>
-                                  <a href="/hosting_confirm/<?php print $environment->platform ?>/platform_git-checkout?create=1" class="btn btn-sm"><i class="fa fa-code-fork"></i> <?php print t('Branch'); ?></a>
+                              <?php if (user_access('create platform git-branch task')): ?>
+                                  <a href="/hosting_confirm/<?php print $environment->platform ?>/platform_git-branch?create=1" class="btn btn-sm"><i class="fa fa-code-fork"></i> <?php print t('Branch'); ?></a>
                               <?php endif; ?>
                               <?php if (user_access('create platform git-tag task')): ?>
                                 <a href="/hosting_confirm/<?php print $environment->platform ?>/platform_git-tag" class="btn btn-sm"><i class="fa fa-tag"></i> <?php print t('Tag'); ?></a>
