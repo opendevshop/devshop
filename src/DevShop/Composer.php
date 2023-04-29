ff4215ec178f9f0386c000b9197b04e8838de419
Jon Pugh <jon@thinkdrop.net>
2023-04-29 09:27:54 -0400

    Change src/DevShop/Control to root.

--- src/DevShop/Composer.php
+++ src/DevShop/Composer.php
@@ -26,21 +26,9 @@ class Composer {
     }
   }
 
-  /**
-   * Run composer install on the DevShop Control sub-project.
-   */
-  static function installControl() {
-    echo "> \n";
-    echo "> Installing src/DevShop/Control ...\n";
-    echo "> \n";
-    $_SERVER['argv'][] = '--working-dir=src/DevShop/Control';
-    $command = implode(' ', $_SERVER['argv']);
-    return self::exec($command);
-  }
-
   static $drupalDevelopmentPaths = [
     'provision' => 'vendor/drupal/provision',
-    'hosting' => 'src/DevShop/Control/web/sites/all/modules/contrib/hosting',
+    'hosting' => 'web/sites/all/modules/contrib/hosting',
   ];
 
   /**
