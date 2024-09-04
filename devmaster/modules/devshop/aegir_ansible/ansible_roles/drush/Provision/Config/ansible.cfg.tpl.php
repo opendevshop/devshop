630d210cee38d7e5ac0ce119032d4dffd6a3fff5
Jon Pugh <jon@thinkdrop.net>
2024-09-04 12:27:33 -0400

    Remove old stuff.

--- devmaster/modules/devshop/aegir_ansible/ansible_roles/drush/Provision/Config/ansible.cfg.tpl.php
+++
@@ -1,5 +0,0 @@
-# WRITTEN BY aegir_ansible module.  Do not edit. Edit /etc/ansible/ansible.cfg if you need to reconfigure ansible.
-[defaults]
-roles_path = "<?php print $roles_path; ?>>"
-stdout_callback  = unixy
-force_color = True
--- tests/test-repo/README.md
+++
@@ -1,3 +0,0 @@
-# Test Repo
-
-This repo exists to confirm that the Git Split process for DevSho is working.
